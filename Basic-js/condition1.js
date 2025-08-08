8c4607e08ac5273ed71e3a0fcff3aa23de7ac395
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-08-08 12:58:59 +0600

    added js file

---
+++ Basic-js/condition1.js
@@ -0,0 +1,8 @@
+//conditional math:
+
+var age = 20 ;
+if (age >= 15 ){
+    console.log("Yes, you can enter here")
+} else {
+    console.log("No, You cant enter here.")
+}
---
+++ Basic-js/if_else.js
@@ -0,0 +1,10 @@
+// condition
+
+var age = 20;
+if (age >= 18) {
+    console.log("You cant enter here. Because you are above 18.")
+} else if (age >= 13) {
+    console.log("Yes , you can enter here.")
+} else {
+    console.log("You can enter only children ride.")
+}
