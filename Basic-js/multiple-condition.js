006577ee76fc44ea185850ffda41ae9fc2a45cb7
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-08-09 21:51:37 +0600

    solved new problem

---
+++ Basic-js/multiple-condition.js
@@ -0,0 +1,16 @@
+git add .
+git commit -m "solved new problem"
+git push
+
+const income = 40000 ;
+const height = 66 ;
+const weight = 50 ;
+const isBCS = true ;
+
+if (income >= 35000 && height >= 66 && weight >=60 ) {
+    console.log("Please , come come you are ready to ")
+} else if (isBCS == true){
+    console.log("I'm ready to make you my son's Husband. Please if you marry my son i will be verry glad")
+} else {
+    console.log("You are disqualified")
+}
