ed2e7046d81a09ec95109e66f232a9749835ac40
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-08-10 20:33:27 +0600

    Done the problem using ternary operation

--- Basic-js/tempCodeRunnerFile.js
+++ Basic-js/tempCodeRunnerFile.js
@@ -1,6 +1,4 @@
-const buingCost = 650 ;
-// if (buingCost >= 500){
-//     console.log("Congratulation, You got a free coke.")
-// }else{
-//     console.log("You have to buy coke. Price 30 tk")
-// }
+const weight = 20 ;
+const height = 1.7 ;
+
+const BMI = (weight /  (height**2)) ;
