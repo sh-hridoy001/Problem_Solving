17691919e6b94f58acc6ac9da7cbae34cb4ad382
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-08-10 20:06:49 +0600

    free drink

---
+++ Basic-js/free-drinks.js
@@ -0,0 +1,17 @@
+// home works 
+
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+const buingCost = 650 ;
+if (buingCost >= 500){
+    console.log("Congratulation, You got a free coke.")
+}else{
+    console.log("You have to buy coke. Price 30 tk")
+}
+
+// lets done it using ternary conditon system
