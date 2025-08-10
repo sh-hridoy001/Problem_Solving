319cab22c1af6a930b5c4f5a12922031e977694e
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-08-10 13:32:03 +0600

    Added ternary condition

--- Basic-js/ternary.js
+++ Basic-js/ternary.js
@@ -5,3 +5,12 @@
  * condition ? do something true  : do something false
  * 
  */
+
+const age = 20 ;
+// if (age >= 18 ){
+//     console.log("You can vote")
+// }else{
+//     console.log("Get out from here. You are under 18.")
+// }
+
+age >= 18 ? console.log("YOU CAN VOTE") : console.log("SORRY , YOU ARE NOT ALLOW") 
---
+++ Basic-js/ternary1.js
@@ -0,0 +1,15 @@
+
+let price = 500 ;
+const isLeader = false ;
+
+// Simple condition operation
+// if (isLeader === true ){
+//     console.log('You dont have to pay , because you are a leader.')
+// }else{
+//     price = price + 100 ;
+//     console.log("You have to pay our leader bill also . ", price )
+// }
+
+
+// Doone the same operation using ternary :
+isLeader == true ? console.log("You dont have to pay , because you are a leader.") : price = price+50  ; console.log("You have to pay : ", price )
