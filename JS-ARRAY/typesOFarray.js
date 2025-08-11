66f5d54fe0e0fe298b456e5055e172ed1bfeb3c2
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-08-11 12:04:49 +0600

    solved array another problem

---
+++ JS-ARRAY/typesOFarray.js
@@ -0,0 +1,16 @@
+
+// string types array-----------
+
+let Name = ['Sazzad', 'Hossain', 'Hridoy'] ;
+console.log(Name.length)
+
+// Index calling
+console.log(Name[2])
+
+// Numbers types Array---------
+console.log(gap)
+
+const numbers = [555, 4582 ,481, 15843, 7845] ;
+console.log(numbers[4])
+console.log(numbers)
+console.log(numbers.length)
