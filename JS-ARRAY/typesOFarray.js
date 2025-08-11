b7c997414fc2bb731c9c7bd9a79b8d54f14ee5c0
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-08-11 12:08:49 +0600

    updated array file

--- JS-ARRAY/typesOFarray.js
+++ JS-ARRAY/typesOFarray.js
@@ -8,9 +8,18 @@ console.log(Name.length)
 console.log(Name[2])
 
 // Numbers types Array---------
-console.log(gap)
 
 const numbers = [555, 4582 ,481, 15843, 7845] ;
 console.log(numbers[4])
 console.log(numbers)
 console.log(numbers.length)
+
+// Mixed Array-------
+
+const mixArray = [1565, 'Hridoy', true, false, 'Sazzad'] ;
+console.log(mixArray)
+// value acces-
+console.log(mixArray.length)
+console.log(mixArray[4])
+
+// 
