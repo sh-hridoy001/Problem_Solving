e6bf0dbeb1d01a54b3cb8af9c794abdf357d5149
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-08-13 18:19:35 +0600

    added new file

--- Basic-js/condition1.js
+++ Basic-js/condition1.js
@@ -1,9 +1,16 @@
 //conditional math:
 
-var age = 20 ;
+// var age = 20 ;
 
-if (age >= 15 ){
-    console.log("Yes, you can enter here")
-} else {
-    console.log("No, You cant enter here.")
+// if (age >= 15 ){
+//     console.log("Yes, you can enter here")
+// } else {
+//     console.log("No, You cant enter here.")
+// }
+
+const age = 20 ;
+if (age >= 18){
+    console.log("Yes , you can vote .")
+}else{
+    console.log("Sorry , you are not eligable for voting .")
 }
