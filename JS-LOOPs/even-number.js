4df59ee9524e4d2cf82585083ff4bda3798a3117
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-08-22 14:22:11 +0600

    added file

---
+++ JS-LOOPs/even-number.js
@@ -0,0 +1,5 @@
+let number = 1;
+while (number <= 100){
+    console.log("Number :" ,number)
+    number = number + 2 ;
+}
--- JS-LOOPs/while-loop.js
+++ JS-LOOPs/while-loop.js
@@ -3,3 +3,9 @@ while(num <= 5){
     console.log("Looppingggg", num);
     num = num + 1;
 }
+
+let number = 0 ;
+while (number <= 10){
+    console.log(number);
+    number = number + 2 ;
+}
