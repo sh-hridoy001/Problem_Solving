80da81f2f743b54cc2ca105c6037782e2be783c4
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-03 23:59:48 +0600

    added js file

--- Javascript/JS-LOOPs/loops-task01/multiplication-table.js
+++ Javascript/JS-LOOPs/loops-task01/multiplication-table.js
@@ -5,11 +5,11 @@ Generate a multiplication table for number 9
  */
 // // Using for loops :
 
-// let num = 9 ;
+let num = 9 ;
 
-// for (let n = 1 ; n<= 10 ; n++){
-//     console.log("9"," * ", n ,"=", num*n)
-// }
+for (let n = 1 ; n<= 10 ; n++){
+    console.log("9"," * ", n ,"=", num*n)
+}
 
 //using while loops :
 
