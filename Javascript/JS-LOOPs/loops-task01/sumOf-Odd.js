fe0d076c145f526e1e033bbefefd212f89a165ba
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-03 13:52:50 +0600

    sum of odd  numbers using loops

---
+++ Javascript/JS-LOOPs/loops-task01/sumOf-Odd.js
@@ -0,0 +1,28 @@
+
+/***
+
+Subtask-1:
+
+Display sum of all the odd numbers from 91 to 129.
+
+ */
+
+
+//using for loops :
+let sum = 0;
+
+for(let num = 91 ; num <= 129 ; num = num + 2){
+    console.log(num);
+    sum = sum + num ;
+}
+console.log("Sum of the these numbers :", sum)
+
+
+/***
+Subtask-2:
+
+Display sum of all the even numbers from 51 to 85.
+
+ */
+
+/*programming hero*/
