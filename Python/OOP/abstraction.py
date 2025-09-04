fe97ba80c7e47c3c4af9a9b2d3df322cbd819eb1
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-04 10:06:11 +0600

    abstraction program added

---
+++ Python/OOP/abstraction.py
@@ -0,0 +1,13 @@
+"...................Abstraction.............................."
+
+print("ABSTRACTION")
+class car:
+    def start(self):
+        print("Car is Started")
+
+    def drive(self):
+        print("Car is moving now,,,")
+
+c = car()
+c.start()
+c.drive()
