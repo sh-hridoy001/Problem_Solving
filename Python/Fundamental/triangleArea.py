b64cf8cc6c82b721f957367e8f94f13846761ef4
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-04 10:19:14 +0600

    triangle area find using CLASS

--- Python/Fundamental/triangleArea.py
+++ Python/Fundamental/triangleArea.py
@@ -10,3 +10,20 @@ def triangle():
     else:
         print("Triangle is not possible.")
 triangle()
+
+
+
+"Using Class         :    -------------"
+import math
+class trianglearea:
+    def __init__(self,a,b,c):
+        if ((a+b)>c and (b+c)>a and (a+c)>b):
+            s = (a+b+c)/2
+            area = math.sqrt(s*(s-a)*(s-b)*(s-c))
+            print("Triagnle are is : ",area)
+        else:
+            print("Triangle is not possible")
+a = int(input("Enter the value of A:"))
+b = int(input("Enter the value of B:"))
+c = int(input("Enter the value of C:"))
+tarea = trianglearea(a,b,c)
