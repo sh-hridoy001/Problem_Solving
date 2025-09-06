81f01c74bac25d2cd7fb31b1840df3a9491bffe0
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-06 11:37:03 +0600

    case in js

--- Javascript/String & Object/case.js
+++ Javascript/String & Object/case.js
@@ -1,5 +1,20 @@
-let name = "MD Sazzad Hossain Hridoy";
-console.log(name);
-console.log(name.toLowerCase());
-console.log(name.toUpperCase());
 
+// // use of case :
+
+// let name = "MD Sazzad Hossain Hridoy";
+// console.log(name);
+// console.log(name.toLowerCase());
+// console.log(name.toUpperCase());
+
+// example :
+
+
+
+let Name = "Sazzad" ;
+let Naam = "sazzad" ;
+
+if (Name === Naam){
+    console.log("Two name are same .");
+}else{
+    console.log("Not, two name aint same .");
+}
