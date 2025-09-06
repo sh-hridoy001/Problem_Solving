219bac8ca37bfde7b46707e2b091a1308ece69bc
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-06 12:50:43 +0600

    js slice , join and concat

---
+++ Javascript/String & Object/concat.js
@@ -0,0 +1,9 @@
+
+//  add first name and last name .
+
+const first = "MD";
+const last = "Sazzad" ;
+console.log(first + " " + last);
+// the same thing we can do using "concat()" :
+const fullname = first.concat(' ') + last.concat()
+console.log(fullname)
