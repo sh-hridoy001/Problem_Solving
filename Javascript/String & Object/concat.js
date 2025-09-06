a4f0d6ee36480e68220b5c6431550434f5560012
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-06 12:53:47 +0600

    js includes uses

--- Javascript/String & Object/concat.js
+++ Javascript/String & Object/concat.js
@@ -7,3 +7,7 @@ console.log(first + " " + last);
 // the same thing we can do using "concat()" :
 const fullname = first.concat(' ') + last.concat()
 console.log(fullname)
+
+
+// we can check any str or num in a variable by using includes :
+console.log(last.includes('a')) //as here have a . the otp will show true.
