47beb7ce0e73d8b05f138bf66a19635de920b3cb
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-06 12:40:37 +0600

    slice in js

--- Javascript/String & Object/slice.js
+++ Javascript/String & Object/slice.js
@@ -1,3 +1,12 @@
+
+//slice() :
 const name = "Sazzad" ;
 const nameofpart = name.slice(2,5);
 console.log(nameofpart)
+
+// splite() :
+const sentece = "I am a good boy.";
+console.log(sentece.split('a'))
+
+const Name = "Rahim, Sahim, Karim, Sahim";
+console.log(Name.split(','))
