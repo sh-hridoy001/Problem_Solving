a0fcfaa43c92091997c48bb66e006302580fd449
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-06 11:15:46 +0600

    string in js

---
+++ Javascript/JS-LOOPs/string.js
@@ -0,0 +1,16 @@
+const district = "Chittagong"
+console.log(district);
+console.log(district.length);
+console.log(district[1]);
+district[2] ="e"; //that's not possible in string .
+
+// string behaves like Array. But main deffirence between array and string is, array er value change kora jay. but string er value change kora jay na. 
+
+// array :
+
+let division = ["Dhaka", "Chittagong", "Rajshahi", "Khulna"];
+console.log(division);
+console.log(division.length);
+console.log(division[2]);
+division[2] = "Rangpur";
+console.log(division)
