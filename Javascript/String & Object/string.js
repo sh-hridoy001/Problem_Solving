a75a11efc00f02829895c139ca1c6a010c8ea6a7
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-06 11:25:48 +0600

    string and object in js

--- Javascript/String & Object/string.js
+++ Javascript/String & Object/string.js
@@ -3,6 +3,7 @@ console.log(district);
 console.log(district.length);
 console.log(district[1]);
 district[2] ="e"; //that's not possible in string .
+// string is immutable :
 
 // string behaves like Array. But main deffirence between array and string is, array er value change kora jay. but string er value change kora jay na. 
 
