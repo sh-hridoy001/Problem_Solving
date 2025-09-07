aa0040d78babdfe7654d1b3c4f98de9d4da1e17f
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-07 14:56:01 +0600

    object in js

---
+++ Javascript/String & Object/object.js
@@ -0,0 +1,15 @@
+
+
+//non-Primitive 
+const indentity = {
+    Name: 'Sazzad Hossain',
+    Roll: '874183',
+    Subject: 'Computer Science',
+    single: true,
+    group: 'science',
+    chapter: ['python','java','js'],
+    exam: {
+        mark: '100',
+        passMark: '40',
+    }
+}
