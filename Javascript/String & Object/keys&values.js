437ce6e5c2bd838fd634b9be2ee0d55166b7ccd4
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-07 15:41:33 +0600

    keys and values in js

---
+++ Javascript/String & Object/keys&values.js
@@ -0,0 +1,15 @@
+const identity = {
+    name: 'sazzad',
+    age: 19,
+    proffession: 'web developer',
+    'marrital status': 'Single',
+    dream: 'become a programmer',
+}
+
+// all property output korar program :
+const keys = Object.keys(identity);
+console.log(keys)
+
+// all value output :
+const value = Object.values(identity);
+console.log(value)
