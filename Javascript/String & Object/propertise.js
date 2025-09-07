5140df22dec27f8b742ead0aa561277221445d3a
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-07 15:19:53 +0600

    property access in js

---
+++ Javascript/String & Object/propertise.js
@@ -0,0 +1,18 @@
+
+const indentity = {
+    name: 'sazzad',
+    age: 19,
+    proffession: 'web developer',
+    'marrital status': 'Single',
+    dream: 'become a programmer',
+}
+
+//for accessing every document 
+console.log(indentity)
+
+// access just a value from this object
+console.log(indentity.name);
+console.log(indentity.proffession);
+
+// if the property defined using space we have to use third bracket
+console.log(indentity['marrital status'])
