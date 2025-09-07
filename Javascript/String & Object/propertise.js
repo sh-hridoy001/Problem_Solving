855309c9c7b2409985ce85458b814441d26ecd49
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-07 15:25:21 +0600

    types of access properties

--- Javascript/String & Object/propertise.js
+++ Javascript/String & Object/propertise.js
@@ -11,8 +11,16 @@ const indentity = {
 console.log(indentity)
 
 // access just a value from this object
+console.log("Direct access in console.")
 console.log(indentity.name);
 console.log(indentity.proffession);
 
+// can acces also using another variable
+console.log("Using defined variable.")
+const sopno = indentity.dream ;
+console.log(sopno);
+
+
 // if the property defined using space we have to use third bracket
-console.log(indentity['marrital status'])
+console.log("If use space in Properties, You have access like this.")
+console.log(indentity['marrital status']);
