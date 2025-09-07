ee15a76b6102ec0df439aed2fd7afa77bd5ef53c
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-07 15:30:41 +0600

    object value change in js

--- Javascript/String & Object/propertise.js
+++ Javascript/String & Object/propertise.js
@@ -24,3 +24,15 @@ console.log(sopno);
 // if the property defined using space we have to use third bracket
 console.log("If use space in Properties, You have access like this.")
 console.log(indentity['marrital status']);
+
+
+
+// we can change also the value of the properties  :
+indentity.age = 20;
+console.log(indentity.age);
+
+indentity['age'] = 21;
+console.log(indentity.age);
+
+indentity['marrital status'] = 'Married';
+console.log(indentity['marrital status'])
