c4440a6a00fd63ee0a47a71bb0c33622e0263c6e
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-09 11:25:50 +0600

    updated sum and sub file

--- Javascript/Function/Basic/Sum&Sub.js
+++ Javascript/Function/Basic/Sum&Sub.js
@@ -6,31 +6,31 @@ function sum(a, b){
 // console.log(sum(5, 6));  for output :-----
 
 let addSum = sum(5, 8);  // another way : ---
-// console.log(addSum)
+console.log(addSum)
 
 
 // Best way to Add  Number : -----
 
 function addNum(num_1, num_2){
-    Sum = num_1 + num_2 ;
+    const Sum = num_1 + num_2 ;
     console.log(Sum);
 }
-// addNum(10,10);
-// addNum(88,41);
-// addNum(20,115);
-// addNum(70,115);
+addNum(10,10);
+addNum(88,41);
+addNum(20,115);
+addNum(70,115);
 
 
 // subtraction in js ;
 
 function subNum(a, b){
-    sub = a - b ;
+    const sub = a - b ;
     console.log(sub)
 }
-// subNum(5, 5);
-// subNum(5, 4);
-// subNum(5, 3);
-// subNum(5, 2);
-// subNum(5, 1);
+subNum(5, 5);
+subNum(5, 4);
+subNum(5, 3);
+subNum(5, 2);
+subNum(5, 1);
 
 
