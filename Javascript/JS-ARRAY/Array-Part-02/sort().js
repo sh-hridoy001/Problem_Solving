fb417f188e9b1652493f6ad6dd57308e21267693
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-09 09:25:15 +0600

    sort in js

--- Javascript/JS-ARRAY/Array-Part-02/sort().js
+++ Javascript/JS-ARRAY/Array-Part-02/sort().js
@@ -1,1 +1,7 @@
 
+// sort() : eta alpabet / number onujayi sajai lekhe . 
+
+const Name = ['MD', 'Sazzad', 'Hossain', 'Hridoy','Developer','Programmer'];
+
+const Nam = Name.sort();
+console.log(Nam)
