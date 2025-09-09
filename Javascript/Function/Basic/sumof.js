97441815240a344e67b4a9ce129b601c9e944a14
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-09 11:04:46 +0600

    subtraction in js

--- Javascript/Function/Basic/sumof.js
+++ Javascript/Function/Basic/sumof.js
@@ -2,10 +2,17 @@
 function sum(a, b){
     return a+b ;
 }
-// for output :-----
-// console.log(sum(5, 6)); 
 
-// another way : ---
+// console.log(sum(5, 6));  for output :-----
 
-let addSum = sum(5, 8);
+let addSum = sum(5, 8);  // another way : ---
 console.log(addSum)
+
+
+// subtraction in js ;
+
+function subNum(a, b){
+    sub = a - b ;
+    console.log(sub)
+}
+subNum(5, 4);
