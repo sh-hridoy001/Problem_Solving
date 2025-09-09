1d2d651c9207492b12dd06522b28cb29ec727f5f
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-09 08:47:07 +0600

    array looping problem solve

---
+++ Javascript/JS-ARRAY/Array-Part-02/practise.js
@@ -0,0 +1,15 @@
+
+
+const dhoniLok = ['Sazzad', 'Hossain', 'Hridoy'];
+
+for (let n =0 ; n < dhoniLok.length ; n++){
+    // console.log(n, dhoniLok[n])
+}
+
+// using while loops :
+
+let n = 0 ;
+while (n < dhoniLok.length){
+    console.log(dhoniLok[n], n);
+    n++ ;
+}
