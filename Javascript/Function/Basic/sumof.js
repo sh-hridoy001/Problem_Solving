ae27d8a34a54862ff5076c09c3c5a5a1d7a8c85b
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-09 10:53:03 +0600

    sum using function

---
+++ Javascript/Function/Basic/sumof.js
@@ -0,0 +1,11 @@
+
+function sum(a, b){
+    return a+b ;
+}
+// for output :-----
+// console.log(sum(5, 6)); 
+
+// another way : ---
+
+let addSum = sum(5, 8);
+console.log(addSum)
---
+++ Javascript/Function/Basic/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+function sum(a, b){
+    return a+b ;
+}
+var num = sum(5, 5);
