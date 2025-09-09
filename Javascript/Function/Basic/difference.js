9f24fcdb5805b2e5f3b2f387bac77b7a456696af
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-09 11:44:02 +0600

    difference in js

---
+++ Javascript/Function/Basic/difference.js
@@ -0,0 +1,9 @@
+
+function difference(num1 , num2){
+    const diff = num1 - num2 ;
+    console.log(num1, num2, 'difference of between these :', diff);
+}
+
+const myAge = 19 ;
+const fatherAge = 45 ;
+difference(fatherAge - myAge);
