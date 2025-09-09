3e91c45bbd799e868fa0248580d0c32fb06f1c3d
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-09 11:17:15 +0600

    double a number in js

--- Javascript/Function/Basic/Sum&Sub.js
+++ Javascript/Function/Basic/Sum&Sub.js
@@ -6,7 +6,7 @@ function sum(a, b){
 // console.log(sum(5, 6));  for output :-----
 
 let addSum = sum(5, 8);  // another way : ---
-console.log(addSum)
+// console.log(addSum)
 
 
 // Best way to Add  Number : -----
@@ -15,10 +15,10 @@ function addNum(num_1, num_2){
     Sum = num_1 + num_2 ;
     console.log(Sum);
 }
-addNum(10,10);
-addNum(88,41);
-addNum(20,115);
-addNum(70,115);
+// addNum(10,10);
+// addNum(88,41);
+// addNum(20,115);
+// addNum(70,115);
 
 
 // subtraction in js ;
@@ -27,8 +27,10 @@ function subNum(a, b){
     sub = a - b ;
     console.log(sub)
 }
-subNum(5, 5);
-subNum(5, 4);
-subNum(5, 3);
-subNum(5, 2);
-subNum(5, 1);
+// subNum(5, 5);
+// subNum(5, 4);
+// subNum(5, 3);
+// subNum(5, 2);
+// subNum(5, 1);
+
+
---
+++ Javascript/Function/Basic/double.js
@@ -0,0 +1,10 @@
+// double a number
+function doubleIt(n){
+    double = n * 2 ;
+    console.log(double);
+}
+
+doubleIt(5);
+doubleIt(25);
+doubleIt(47);
+doubleIt(8);
