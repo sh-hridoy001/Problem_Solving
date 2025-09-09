ef3c5592c296196e08b6b3036410d6a33b96b551
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-09 10:25:22 +0600

    array file updated

--- Javascript/JS-ARRAY/Array-Part-01/array-update.js
+++ Javascript/JS-ARRAY/Array-Part-01/array-update.js
@@ -3,4 +3,6 @@
 let name = ['Sazzad', 'Hossain', 'Hriody']
 name[3] = " MITHILA";
 console.log(name)
+name[4] = "Sumaiya";
+console.log(name)
 // by using index we can add another list and also we can update or change the value of any index you want.
