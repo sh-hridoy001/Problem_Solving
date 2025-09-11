640bac4fb8d8c76d9cdd2f593911460d7ab193a0
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-11 08:37:50 +0600

    Added evenOdd.js file

--- Javascript/Function/Math-Operation/evenOdd.js
+++ Javascript/Function/Math-Operation/evenOdd.js
@@ -1,1 +1,15 @@
-console.log('hello ')
+
+// FInd out even and odd number :
+
+function even(n) {
+    if (n%2 === 0){
+        console.log("This is and even Number :",n)
+    }
+    else{
+        console.log("Odd number :",n)
+    }
+}
+even(5);
+even(14);
+even(8);
+// git add Javascript/function/Math-Operation/evenOdd.js
