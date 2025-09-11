cf8b6707207be9a8227e7930e3f6cce8a54aadd5
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-11 10:08:28 +0600

    updated some file

---
+++ Javascript/Function/Math-Operation/sumOf-Array.js
@@ -0,0 +1,9 @@
+
+function sumOFarray(classRoll){
+
+}
+
+const result = sumOFarray();
+console.log("Sum of numbers :",result)
+
+
--- Javascript/JS-ARRAY/Array-Part-02/tempCodeRunnerFile.js
+++ Javascript/JS-ARRAY/Array-Part-02/tempCodeRunnerFile.js
@@ -1,5 +1,1 @@
-const revese_num = [];
-for (let i = 0 ; i < numbers.length ; i ++){
-    const num = numbers[i];
-    revese_num.unshift(num);
-}
+split("")
