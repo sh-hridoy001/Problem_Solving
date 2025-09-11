9c7feffdc4cd925d3e73bd8d1dc90d12af666ea5
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-11 09:01:20 +0600

    Add variousPorams JavaScript implementation

---
+++ Javascript/Function/Math-Operation/various-porams.js
@@ -0,0 +1,13 @@
+
+
+function evenSizedstr(str){
+    const size = str.length ;
+    console.log(str,'. Ekhane', size ,'ta word ache.')
+    if(str %2===0){
+        console.log("Even String size")
+    }else{
+        console.log("Odd str size.")
+    }
+}
+
+evenSizedstr('Chittagong');
