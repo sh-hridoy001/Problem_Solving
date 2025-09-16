6152bc42a36f0dc6c3cd2ce19529b3aeb5e9a817
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-16 10:43:37 +0600

    added

---
+++ C++/CodeShef/multiple_output.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // add two cout statements
+
+    cout<< 3+4 << 2+1 ; 
+}
---
+++ C++/separet_otp.cpp
@@ -0,0 +1,19 @@
+// author: Sazzad
+
+
+// Task
+// Write a program which does the following
+
+// Output the sum of 3 and 4 using cout.
+// Output the sum of 1 and 2 using cout, but on a new line.
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  cout<< 4+3 <<endl ;
+  cout<< 2+1 ;
+  return 0;
+}
