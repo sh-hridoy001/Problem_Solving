2252e537d711457c769b4e542d0b205ce8d1fa6d
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-17 11:52:08 +0600

    added

---
+++ C++/CodeShef/declareAvariable.cpp
@@ -0,0 +1,17 @@
+// Task
+// Write a program which does the following:
+
+// Create an integer variable named number.
+// Assign the value 19 to number.
+// Output the value of number.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declare a variable
+    int number = 19;
+    
+    // Print the variable
+    cout << number ;
+}
