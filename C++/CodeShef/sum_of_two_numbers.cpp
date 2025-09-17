980ecfa4ab91100ebaf591ee1e27a9e4802a958b
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-17 11:55:30 +0600

    added

---
+++ C++/CodeShef/sum_of_two_numbers.cpp
@@ -0,0 +1,16 @@
+//Write a program which does the following
+
+// Declare two integer variables a and b
+// Assign the value 23 to a and 20 to b
+// Output the sum of a and b to the console 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a = 23 ;
+    int b = 20 ;
+    cout<< a + b <<endl ;
+
+}
