737940857115b1444325eea7b56bf403f7fa45c0
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-17 12:00:10 +0600

    added

---
+++ C++/CodeShef/area.cpp
@@ -0,0 +1,18 @@
+// Write a program which does the following
+
+// Find out and display the area of a rectangle of sides 45 and 76 respectively.
+// Declare variables length, width and area and assign the relevant values to them
+// Make sure to add some text, so that the person reading the output knows what the output means.
+
+// author: Sazzad
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int sides = 45 ;
+  int width = 76 ;
+  int area = sides * width ;
+  cout << "The Area of the given rectangle is " << area << endl;
+  return 0;
+}
