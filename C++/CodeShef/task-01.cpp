95987dac3308486634597b1af2a483be46ed87d8
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-17 12:14:47 +0600

    added

---
+++ C++/CodeShef/task-01.cpp
@@ -0,0 +1,21 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int a = 1 ;
+	int b = 2 ;
+	int c = 3 ;
+	int d = 4 ;
+	int e = 5 ;
+	
+	cout << a << " - " <<a*a << endl;
+	cout << b << " - " <<b*b << endl;
+	cout << c << " - " <<c*c << endl;
+	cout << d << " - " <<d*d << endl;
+	cout << e << " - " <<e*e << endl;
+	
+
+}
