99227df7fd72a85bc0fe196187aa201e0feb07aa
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-17 12:13:31 +0600

    added

---
+++ C++/CodeShef/double_data-type.cpp
@@ -0,0 +1,20 @@
+// Task
+// Write a program which does the following:
+
+// Find the area of a circle whose radius is 8.9. Take pi = 3.14
+// Declare variables radius, pi and area and assign the relevant values to them
+// Formula for area of a circle is Pi * radius * radius
+
+// author: Sazzad
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  double radius = 8.9 ;
+  double pi = 3.14 ;
+  double area = pi * radius*radius ;
+  cout << "The Area of the given Circle is " << area << endl;
+  
+  return 0;
+}
