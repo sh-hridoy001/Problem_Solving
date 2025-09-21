de1e4bd6758b30088bb85fc94633cc0bafa7909c
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-21 15:53:50 +0600

    added

---
+++ C++/Loops/highest_factor.cpp
@@ -0,0 +1,13 @@
+// // author: Sazzad
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int n ; cout<< "Enter N: " ; cin>> n ;
+//     int f = 1 ;
+//     for(int i =1 ; i<n ; i++){
+//         if(n%i ==0) f= i ;
+//     }
+//     cout<< f ;
+// }
