66ac8936c5b98a54da827bb1d02646a13aecf16e
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-21 11:19:27 +0600

    added

--- C++/Loops/print_the_table_of_19.cpp
+++ C++/Loops/print_the_table_of_19.cpp
@@ -3,6 +3,23 @@
 using namespace std;
 
 int main()
+{
+    for(int i = 19 ; i<= 190 ; i++){
+        if(i%19 ==0){
+            cout<< i <<" ";
+        }
+    }
+  return 0;
+}
+
+
+/// 2nd option
+
+// author: Sazzad
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
 {   
     for(int i = 19 ; i<=190 ; i = i+19){
         cout<< i <<endl;
