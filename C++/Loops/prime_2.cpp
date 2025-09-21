977ca9a37d6693fc3e3e7090f13214a7e204044a
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-21 10:58:09 +0600

    added

--- C++/Loops/prime_2.cpp
+++ C++/Loops/prime_2.cpp
@@ -6,8 +6,7 @@ int main()
 {   int n = 12 ;
     for(int i = 1 ; i<=(n/2) ; i++ ){
         if(n%i == 0){
-            cout<< i <<endl ;
-            
+            cout<< i <<endl ;         
         }
     }
   return 0;
