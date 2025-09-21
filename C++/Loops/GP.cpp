a6adeeb8d046078feeb808783f86e12fe894e79a
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-21 12:13:20 +0600

    added

---
+++ C++/Loops/GP.cpp
@@ -0,0 +1,15 @@
+// author: Sazzad
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n ; cout<< "Enter N: "; cin>> n ;
+    int a = 1 ;
+    for(int i = 1 ; i<=n ; i++){
+        cout<<a <<", " ;
+        a = a*2 ;
+    }
+
+  return 0;
+}
