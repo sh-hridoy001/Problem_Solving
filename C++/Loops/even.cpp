6f18f496e0f1de57e28a2a2e36ba82295d1c6021
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-21 10:55:11 +0600

    added

---
+++ C++/Loops/even.cpp
@@ -0,0 +1,15 @@
+
+// author: Sazzad
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{   
+    int n = 100 ;
+    for(int i = 1 ; i <=n ; i++){
+        if( i%2 == 0){
+            cout<< i << " ";
+        }
+    }
+  return 0;
+}
