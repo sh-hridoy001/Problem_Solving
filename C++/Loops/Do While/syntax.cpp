2a6b3bab05a4da1aaa6eae4e5fc5b3a8b4d1124f
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-22 11:28:30 +0600

    added

---
+++ C++/Loops/Do While/syntax.cpp
@@ -0,0 +1,13 @@
+// author: Sazzad
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i = 11 ;
+    do{
+        cout<< i << " " ;
+        i++ ;
+    }while(i<=10) ;
+  return 0;
+}
