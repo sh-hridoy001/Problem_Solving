4e2ee162a82afb58dc291555bbaf5ceb5777e8da
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-23 09:54:05 +0600

    added

---
+++ C++/Loops/While Loop/reverse.cpp
@@ -0,0 +1,19 @@
+// author: Sazzad
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n ; cout<< "Enter N: " ; cin>> n ;
+    int i = 1 ;
+    int rev= 0 ;
+    while(n!=0){
+        int ld = n%10 ;
+        rev = rev*10 ;
+        rev = rev + ld ;
+        n = n/10 ;
+        i++ ;
+    }
+    cout<< rev ;
+  return 0;
+}
