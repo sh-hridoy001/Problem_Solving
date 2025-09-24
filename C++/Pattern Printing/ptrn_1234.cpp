3a4bb4bd8e652f0beedffbee3d9455c40b8155b4
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-24 09:18:28 +0600

    added

---
+++ C++/Pattern Printing/ptrn_1234.cpp
@@ -0,0 +1,15 @@
+// author: Sazzad
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{   
+    int n; cout<< "Enter N: "; cin>> n;
+    for(int x = 1 ; x<=n; x++){
+        for(int i =1 ; i<=n; i++){
+        cout<< i <<" " ;
+    }
+    cout<<endl ;
+    }
+  return 0;
+}
