efddc056dad02023c5a618baca4ecd4fbedf3d7e
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-24 09:22:00 +0600

    added

---
+++ C++/Pattern Printing/user_input.cpp
@@ -0,0 +1,14 @@
+// author: Sazzad
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{   
+    int n ; cout<< "Enter N: "; cin>> n ;
+    int m ; cout<< "Enter N: "; cin>> m ;
+
+    for(int i =1 ; i<=n ; i++){
+        cout<<"* " ;
+    }
+  return 0;
+}
