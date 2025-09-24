f703a76705952c87b37467031be2add677e1d137
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-24 09:26:08 +0600

    added

--- C++/Pattern Printing/user_input.cpp
+++ C++/Pattern Printing/user_input.cpp
@@ -4,11 +4,14 @@ using namespace std;
 
 int main()
 {   
-    int n ; cout<< "Enter N: "; cin>> n ;
-    int m ; cout<< "Enter N: "; cin>> m ;
+    int n ; cout<< "Enter column number: "; cin>> n ;
+    int m ; cout<< "Enter row number: "; cin>> m ;
 
-    for(int i =1 ; i<=n ; i++){
+    for(int x =1 ; x<=m ; x++){
+        for(int i =1 ; i<=n ; i++){
         cout<<"* " ;
+        }
+        cout<< endl ;
     }
   return 0;
 }
