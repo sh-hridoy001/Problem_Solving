a8e44a76b9b5e13df7cd42abfd9289035d658471
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-24 10:51:45 +0600

    added

---
+++ C++/Pattern Printing/1111.cpp
@@ -0,0 +1,15 @@
+// author: Sazzad
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n; cout<< "Enter N: "; cin>> n ;
+    for(int i =1; i<=n; i++){
+        for(int x=1; x<=n ; x++){
+            cout<< i <<" " ;
+        }
+        cout<< endl ;
+    }
+  return 0;
+}
