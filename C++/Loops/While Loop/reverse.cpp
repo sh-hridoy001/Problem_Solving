54f7aac0f82e7c79509c82b79272faaef8e60b3f
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-24 09:05:35 +0600

    added

--- C++/Loops/While Loop/reverse.cpp
+++ C++/Loops/While Loop/reverse.cpp
@@ -6,12 +6,18 @@ int main()
 {
     int n ; cout<< "Enter N: " ; cin>> n ;
     int rev= 0 ;
+    int number = 0 ;
     while(n!=0){
         int ld = n%10 ;
+        number = number + ld ;
         rev = rev*10 ;
         rev = rev + ld ;
         n = n/10 ;
     }
-    cout<< rev ;
+    cout<< rev <<endl ;
+    cout<< number + rev ;
   return 0;
 }
+
+
+// cant finding the sum of n and reverse number.
