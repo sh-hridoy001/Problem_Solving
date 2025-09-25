56bc30a38e327e9774e28ca2c9e8b007326a1915
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-25 13:08:31 +0600

    added

--- C++/Pattern Printing/Triangle.cpp/4_43_432.cpp
+++ C++/Pattern Printing/Triangle.cpp/4_43_432.cpp
@@ -7,7 +7,7 @@ int main()
     int n; cout<<"Enter N: "; cin>> n;
     for(int i=1; i<=n; i++){ // i is for row,, how many row will be.
         for(int j=1; j<=i ; j++){ // j is for column ,,, like:- ....... 1 2 3 4 .........  A B C D ........
-            cout<< j <<" ";
+            cout<< n-j+1 <<" ";
         }
         cout<<endl ;
     }
