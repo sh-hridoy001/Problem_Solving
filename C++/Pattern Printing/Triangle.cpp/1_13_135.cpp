1502e38a0d2ee6a00ef602a69ad2d2aaab989b9f
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-25 13:18:42 +0600

    added

--- C++/Pattern Printing/Triangle.cpp/1_13_135.cpp
+++ C++/Pattern Printing/Triangle.cpp/1_13_135.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main()
 {
-
+    int n; cout<<"Enter N: "; cin>> n;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout<< 2*j-1 <<" " ;
+        }
+        cout<<endl ;
+    }
   return 0;
 }
