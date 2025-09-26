320c8f7598e528e45b9980d4159f95f1518f9cb8
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-26 17:53:18 +0600

    added

--- C++/Pattern Printing/Triangle.cpp/practise.cpp
+++ C++/Pattern Printing/Triangle.cpp/practise.cpp
@@ -7,7 +7,7 @@ int main()
     int n; cout<<"Enter N:"; cin>> n;
     for(int i=1; i<=n; i++){
         for(int j=1; j<=i;j++){
-            cout<<j <<" ";
+            cout<<(char)(j+64) <<" ";
 
         }
         cout<< endl ;
