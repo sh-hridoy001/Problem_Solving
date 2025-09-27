8dadc13a7388a423030103c9d79292738e73c869
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-27 22:36:42 +0600

    added

---
+++ C++/Pattern Printing/Triangle.cpp/hollow_ractangular.cpp
@@ -0,0 +1,20 @@
+ // author: Sazzad
+ #include <bits/stdc++.h>
+ using namespace std;
+ 
+ int main()
+ {
+    int n; cout<<"Enter N: " ; cin>> n;
+    int m; cout<<"Enter M: " ; cin>> m;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            if(i==1 || j==1 || i==n || j==m){
+                cout<< "* " ;
+            }else{
+                cout<< "  " ;
+            }
+        }
+        cout<<endl ;
+    }
+   return 0;
+ }
