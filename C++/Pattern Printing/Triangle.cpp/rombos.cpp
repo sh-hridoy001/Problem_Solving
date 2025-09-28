225af89414e55039dde758475e012489b189b8c9
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-28 11:55:04 +0600

    added

---
+++ C++/Pattern Printing/Triangle.cpp/rombos.cpp
@@ -0,0 +1,18 @@
+// author: Sazzad
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n; cout<<"Enter N: " ; cin>> n;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n-i ;j++){
+            cout<<" " ;
+        }
+        for(int x=1; x<=n ; x++){
+            cout<<"* " ;
+        }
+        cout<< endl ;
+    }
+  return 0;
+}
