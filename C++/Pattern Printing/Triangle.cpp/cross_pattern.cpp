db6f08ba9465ef31dde4e5df9599f3bea4189598
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-28 08:34:27 +0600

    added

---
+++ C++/Pattern Printing/Triangle.cpp/cross_pattern.cpp
@@ -0,0 +1,19 @@
+// author: Sazzad
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n; cout<< "Enter N: "; cin>> n;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            if(j == i || j == (n + 1 - i)){
+                cout<<"* " ;
+            }else{
+                cout<<"  " ;
+            }
+        }
+        cout<< endl ;
+    }
+  return 0;
+}
