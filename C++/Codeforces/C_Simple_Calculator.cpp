844db18c78c841c66e5af7711e62de210d30f1e3
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-09-30 14:43:42 +0600

    added

---
+++ C++/Codeforces/C_Simple_Calculator.cpp
@@ -0,0 +1,13 @@
+// author: Sazzad
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long a, b;
+    cin >> a >> b;
+    cout << a << " + " << b << " = " << a + b << endl;
+    cout << a << " * " << b << " = " << a * b << endl;
+    cout << a << " - " << b << " = " << a - b << endl;
+    return 0;
+}
