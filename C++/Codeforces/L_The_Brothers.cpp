8b01c5cb000699cb01cdaf4abb1ab1e6232b6f7e
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-10-01 12:16:04 +0600

    added

--- C++/Codeforces/L_The_Brothers.cpp
+++ C++/Codeforces/L_The_Brothers.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 int main()
 {
-  
+  string name_1, name_1_last, name_2, name_2_last;
+  cin >> name_1 >> name_1_last;
+  cin >> name_2 >> name_2_last;
+  if (name_1_last == name_2_last)
+
+  {
+    cout << "ARE Brothers \n";
+  }
+  else
+  {
+    cout << "NOT\n";
+  }
   return 0;
 }
