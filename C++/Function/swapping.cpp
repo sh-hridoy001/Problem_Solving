9ecc5ab01e7e413e02a0d0a632f1f705a75a39c4
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-10-01 14:38:59 +0600

    added

--- C++/Function/swapping.cpp
+++ C++/Function/swapping.cpp
@@ -6,10 +6,17 @@ int main()
 {
     int x, y, temp;
     cin >> x >> y;
-    cout << x << " " << y <<endl;
-    temp = x;
-    x = y;
-    y = temp ;
+
+    // cout << x << " " << y <<endl;
+    // temp = x;
+    // x = y;
+    // y = temp ;
+    // cout << x << " " << y;
+
+    x = x+y ;
+    y = x-y ;
+    x = x-y ;
     cout << x << " " << y;
+
     return 0;
 }
