f3b580de26fa3a1a3fe80f0708fdba3258842ad4
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-10-04 09:48:23 +0600

    added

---
+++ C++/Array/reverse_arr_loops.cpp
@@ -0,0 +1,12 @@
+// author: Sazzad
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {1,2,3,4,5};
+    for(int i=4; i>=0; i--){
+        cout<<arr[i] <<" " ;
+    }
+  return 0;
+}
