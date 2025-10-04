def8cb0344f60b02688ffb179f3b06d5bc1870ff
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-10-04 09:44:58 +0600

    added

--- C++/Array/initialization.cpp
+++ C++/Array/initialization.cpp
@@ -5,9 +5,18 @@ using namespace std;
 int main()
 {
     int arr[5] = {1, 2, 3 , 4 ,5} ;
-    
+
     for(int i=0 ; i<=4 ; i++){
         cout<<arr[i]*2 <<" " ;
+    }
+
+    // update any data of a array uisng index number ----
+    cout<<endl ;
+    
+    arr[0] = 15 ;
+    arr[4] = 12 ;
+    for(int i=0; i<=4; i++){
+        cout<<arr[i] <<" " ;
     }
   return 0;
 }
