3d5afca67847b4f1d80d325106a3912db06823bb
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-10-04 09:41:10 +0600

    added

---
+++ C++/Array/initialization.cpp
@@ -0,0 +1,13 @@
+// author: Sazzad
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {1, 2, 3 , 4 ,5} ;
+    
+    for(int i=0 ; i<=4 ; i++){
+        cout<<arr[i]*2 <<" " ;
+    }
+  return 0;
+}
