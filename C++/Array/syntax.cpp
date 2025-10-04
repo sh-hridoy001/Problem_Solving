72260cc9ec1841cdd59b64145dc185d6365b0100
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-10-04 09:29:42 +0600

    added

---
+++ C++/Array/syntax.cpp
@@ -0,0 +1,21 @@
+// author: Sazzad
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[5];
+    arr[0] = 50;
+    arr[1] = 46;
+    arr[2] = 34;
+    arr[3] = 86;
+    arr[4] = 45;
+
+    cout << arr[0] << " ";
+    cout << arr[1] << " ";
+    cout << arr[2] << " ";
+    cout << arr[3] << " ";
+    cout << arr[4] << " ";
+
+    return 0;
+}
