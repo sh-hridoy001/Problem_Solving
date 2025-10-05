1f6c1550c4d787ffe3bd32c2b5ed664c5973a697
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-10-05 10:30:14 +0600

    added

--- C-Programming/Module_01/variable.c
+++ C-Programming/Module_01/variable.c
@@ -5,6 +5,13 @@
 int main()
 {
     printf("Data Type In C Programming \n") ;
-    printf("INT, Float, Char, Boolean, Double, Long Long") ;
+    printf("INT, Float, Char, Boolean, Double, Long Long\n") ;
+    int integer = 500 ; //(INT, FLOAT) 4 bit memory use..... Char use only 1 bit ..
+    float f = 3.1416;
+    char c = 'H' ;
+    printf("%d\n", integer) ;
+    printf("%f\n", f);
+    printf("%c\n", f) ;
+
     return 0;
 }
