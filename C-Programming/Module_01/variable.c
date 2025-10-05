22f8d10973f2a735eee5a8b01e55d62b091c20dc
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-10-05 09:50:35 +0600

    added

---
+++ C-Programming/Module_01/variable.c
@@ -0,0 +1,10 @@
+//      <-------- data - type ------------>
+// int , float , char , boolean, double , long long
+
+#include <stdio.h>
+int main()
+{
+    printf("Data Type In C Programming \n") ;
+    printf("INT, Float, Char, Boolean, Double, Long Long") ;
+    return 0;
+}
