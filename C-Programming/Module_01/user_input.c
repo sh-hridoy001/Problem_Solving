a29bb5ceb4be93c3d553a19094c1bf5f80097ce8
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-10-05 10:53:20 +0600

    added

--- C-Programming/Module_01/user_input.c
+++ C-Programming/Module_01/user_input.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
 int main(){
-    int num1 ; 
-    printf("Enter A Number : ") ;
-    scanf("%d", &num1);
-    printf("%d", num1) ;
+    // int num1 ; 
+    // printf("Enter A Number : ") ;
+    // scanf("%d", &num1);
+    // printf("%d", num1) ;
+
+    int num ;
+    float f ;
+    char c ;
+
+    scanf("%d %f %c", &num, &f, &c);
+    printf("%d %.2f %c", num, f, c) ;
     return 0 ;
 }
