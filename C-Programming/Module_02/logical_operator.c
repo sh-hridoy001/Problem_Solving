706f2a4b7ee60931bf7cbdd05c6a79b3dd1602a7
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-10-06 09:22:31 +0600

    added

---
+++ C-Programming/Module_02/logical_operator.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    // some extra  <----- && (and) || (or) ! (not) ----->
+
+    return 0;
+}
--- C-Programming/Module_02/relational_operators.c
+++ C-Programming/Module_02/relational_operators.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main()
+{
     // <----- <, <= , >, >= , ==, != ---->
     // these are called relatinal operators .
-    // some extra  <----- && (and) || (or) ! (not) ----->
 
-    return 0 ;
+    return 0;
 }
