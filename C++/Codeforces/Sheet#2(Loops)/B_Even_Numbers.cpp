405681a7bbace161c11ff9454cb9adba8b138f92
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-10-08 17:25:20 +0600

    added

---
+++ C++/Codeforces/Sheet#2(Loops)/B_Even_Numbers.cpp
@@ -0,0 +1,21 @@
+// author: Sazzad
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n < 2)
+    {
+        cout << "-1";
+    }
+    else
+    {
+        for (int i = 2; i <= n; i += 2)
+        {
+            cout << i << endl;
+        }
+    }
+    return 0;
+}
