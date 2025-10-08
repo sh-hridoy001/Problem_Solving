dfd6d4f2ff60d82e3323bdb71fc9478f32f18246
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-10-08 16:58:26 +0600

    added

--- C-Programming/Module_03/Quiz/practice_2.c
+++ C-Programming/Module_03/Quiz/practice_2.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-
-    int i=0, j=0;
-    for(int j=1; j<=5; j++){
-        i++ ;
+int main()
+{
+    int i = 0, j = 0;
+    for (j = 1; j <= 5; j++)
+    {
+        i++;
     }
-    printf("%d\n", j);
-    printf("%d", i);
-    return 0 ;
+    printf("%d", j);
+    return 0;
 }
---
+++ C-Programming/Module_03/Quiz/practice_7.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+
+    int i = 5 ;
+    while(i<10){
+        printf("i");
+        i++ ;
+    }
+    return 0 ;
+}
