2d92da5fca3864aa85540945cea32614433aee0e
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-10-10 00:44:40 +0600

    added

---
+++ Multiply.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    long long int a, b;
+    scanf("%lld %lld", &a, &b);
+    long long int multiplication = a * b;
+    printf("%lld", multiplication);
+
+    return 0;
+}
