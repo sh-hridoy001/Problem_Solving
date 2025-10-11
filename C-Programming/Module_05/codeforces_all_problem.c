a5c3112f7087cf9555ecc8db8f62da288f83a95f
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-10-11 21:48:53 +0600

    added

---
+++ C-Programming/Module_05/codeforces_all_problem.c
@@ -0,0 +1,176 @@
+
+// ------------------------------I-No ----------------------------
+
+// #include <stdio.h>
+// int main()
+// {
+//     int a, b;
+//     scanf("%d %d", &a, &b);
+//     if(a>=b){
+//         printf("Yes\n");
+//     }else{
+//         printf("No\n");
+//     }
+//     return 0;
+// }
+
+
+// -------------------------------- J- NO ---------------------------------
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a, b;
+//     scanf("%d %d", &a, &b);
+
+//     if(a%b==0 || b%a==0){
+//         printf("Multiples");
+//     }else{
+//         printf("No Multiples");
+//     }
+//     return 0;
+// }
+
+
+
+// -------------------------------- K- NO -------------------------------
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a, b, c;
+//     scanf("%d %d %d", &a, &b, &c);
+
+//     // Minimum-------
+//     if (a <= b && a <= c)
+//     {
+//         printf("%d ", a);
+//     }
+//     else if (b <= a && b <= c)
+//     {
+//         printf("%d ", b);
+//     }
+//     else
+//     {
+//         printf("%d ", c);
+//     }
+
+//     // Maximum-----
+//     if (a >= b && a >= c)
+//     {
+//         printf("%d\n", a);
+//     }
+//     else if (b >= a && b >= c)
+//     {
+//         printf("%d\n", b);
+//     }
+//     else
+//     {
+//         printf("%d\n", c);
+//     }
+//     return 0;
+// }
+
+// -------------------------------- M- NO ----------------------
+
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     char x;
+//     scanf("%c", &x);
+//     int digit = (int)x;
+
+//     if (digit >= 65 && digit <= 90)
+//     {
+//         char latter = (char)digit + 32;
+//         printf("%c", latter);
+//     }
+//     else if (digit >= 97 && digit <= 122)
+//     {
+//         char latter = (char)digit - 32;
+//         printf("%c", latter);
+//     }
+
+//     return 0;
+// }
+
+
+// -------------------------------- N- NO ----------------------------
+
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     char x;
+//     scanf("%c", &x);
+//     int digit = (int)x;
+
+//     if (digit >= 65 && digit <= 90)
+//     {
+//         printf("ALPHA\n");
+//         printf("IS CAPITAL\n");
+//     }
+//     else if (digit >= 97 && digit <= 122)
+//     {
+//         printf("ALPHA\n");
+//         printf("IS SMALL\n");
+//     }else{
+//         printf("IS DIGIT\n");
+//     }
+
+//     return 0;
+// }
+
+
+// -------------------------------- J- NO --------------------------
+
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     char x;
+//     scanf("%c", &x);
+//     int digit = (int)x;
+
+//     if (digit >= 65 && digit <= 90)
+//     {
+//         char latter = (char)digit + 32;
+//         printf("%c", latter);
+//     }
+//     else if (digit >= 97 && digit <= 122)
+//     {
+//         char latter = (char)digit - 32;
+//         printf("%c", latter);
+//     }
+
+//     return 0;
+// }
+
+
+// -------------------------------- P- NO ----------------------
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int last_digit = n / 1000;
+//     if (last_digit % 2 == 0)
+//     {
+//         printf("EVEN\n");
+//     }
+//     else
+//     {
+//         printf("ODD\n");
+//     }
+
+//     return 0;
+// }
+
+// // accepted codeforces*
