aadcf90697de1836a06486a25eb10f1831c9db35
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-10-11 11:02:24 +0600

    added

--- C-Programming/Codeforces/I_Welcome_for_you_with_Conditions.c
+++ C-Programming/Codeforces/I_Welcome_for_you_with_Conditions.c
@@ -1,15 +1,35 @@
-#include<stdio.h>
+// #include<stdio.h>
+
+// int main()
+// {
+//     int a, b;
+//     scanf("%d %d",&a, &b);
+
+//     if(a>b || a==b){
+//         printf("Yes\n");
+
+//     }else{
+//         printf("No");
+//     }
+//     return 0;
+// }
+
+//accepted it also ..
+
+
+
+#include <stdio.h>
 
 int main()
 {
     int a, b;
-    scanf("%d %d",&a, &b);
-
-    if(a>b || a==b){
+    scanf("%d %d", &a, &b);
+    if(a>=b){
         printf("Yes\n");
-
     }else{
-        printf("No");
+        printf("No\n");
     }
     return 0;
 }
+
+// accepted codeforces*
