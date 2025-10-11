ce4e131335c8e19e14b85762b863873924649b00
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-10-11 21:38:58 +0600

    added

--- C++/Codeforces/Sheet#1(Data-type & Condition)/K_Max_and_Min.cpp
+++ C++/Codeforces/Sheet#1(Data-type & Condition)/K_Max_and_Min.cpp
@@ -4,8 +4,10 @@ using namespace std;
 
 int main()
 {
-    int a , b , c ;
-    cin>> a >> b >> c ;
-    cout<< min({a,b,c}) <<" " <<max({a,b,c}) ;
+  int a, b, c;
+  cin >> a >> b >> c;
+  cout << min({a, b, c}) << " " << max({a, b, c});
   return 0;
 }
+
+//accepted in codeforces
