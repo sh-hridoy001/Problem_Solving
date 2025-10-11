9c7da35732b96404b2994879ad0bf078dd0fc5c7
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-10-11 10:14:46 +0600

    added

---
+++ E_Max.cpp
@@ -0,0 +1,13 @@
+// author: Sazzad
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, numbers; cin>>n;
+    for(int i=1; i<=n; i++){
+        cin>>numbers ;
+    }
+    cout<<numbers ;
+    return 0;
+}
