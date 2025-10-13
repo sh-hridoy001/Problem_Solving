4f25484ba2c2b12cfa22cc312698c2e2e70302e7
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-10-13 12:45:14 +0600

    added

--- Java/basics/HelloWorld.java
+++ Java/basics/HelloWorld.java
@@ -1,4 +1,4 @@
-package Java;
+package Java.basics;
 
 public class HelloWorld {
     public static void main(String[] args) {
--- Java/basics/sazzad.java
+++ Java/basics/sazzad.java
@@ -1,4 +1,4 @@
-package Java;
+package Java.basics;
 
 import java.util.Scanner;
 
