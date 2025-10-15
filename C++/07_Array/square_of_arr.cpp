c44d10594eed5abb520c3d3e15967eae4b531734
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-10-16 00:19:20 +0600

    added

---
+++ C++/07_Array/square_of_arr.cpp
@@ -0,0 +1,17 @@
+// author: Sazzad
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int j = 1; j <= n; j++)
+    {
+        cout << j*j << " ";
+    }
+
+    return 0;
+}
