f2224ef3a505c94e53004ba229b0c8fbde55aa42
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-10-18 00:00:16 +0600

    added

---
+++ C-Programming/Practise File/array_operation.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    // array input ---------
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int position, value;
+    scanf("%d %d", &position, &value);
+
+    for (int i = n; i >= position + 1; i--)
+    {
+        arr[i] = arr[i - 1];
+    }
+    arr[position] = value;
+    for (int i = 0; i <= n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
