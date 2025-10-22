1f74aba07e8121692d21db302156e2405b2a301b
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-10-22 11:32:04 +0600

    added

--- C-Programming/Module_11/copy_str.c
+++ C-Programming/Module_11/copy_str.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
-#include<string.h>
+#include <string.h>
 
 int main()
 {
     char a[101], b[101];
-    scanf("%s %s", &a, &b);
-    int length = strlen(a) ;
-    for(int i=0; i<length; i++){
-        a[i] = b[i] ;
+    scanf("%s %s", a, b);
+    int length = strlen(b);
+    for (int i = 0; i <= length; i++)
+    {
+        a[i] = b[i];
     }
     printf("%s %s", a, b);
 
     return 0;
 }
+
+// eta codeforces er problem na---
