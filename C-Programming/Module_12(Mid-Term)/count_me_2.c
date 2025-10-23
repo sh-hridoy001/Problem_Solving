1228b028d7472003bc7405d1e79784087e3f76cc
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-10-24 00:05:47 +0600

    added

---
+++ C-Programming/Module_12(Mid-Term)/count_me_2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[100001];
+    scanf("%s", str);
+    int lenght = strlen(str);
+
+    int consonant = 0;
+    for (int i = 0; i < lenght; i++)
+    {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+        {
+            continue;
+        }
+        else
+        {
+            consonant++;
+        }
+    }
+    printf("%d", consonant);
+
+    return 0;
+}
