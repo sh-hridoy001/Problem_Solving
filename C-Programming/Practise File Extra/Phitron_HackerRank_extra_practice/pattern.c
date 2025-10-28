2f02dd147fed025a4a9e3b5637ea5585cfbf79c9
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-10-28 12:18:38 +0600

    added

--- C-Programming/Practise File Extra/Phitron_HackerRank_extra_practice/pattern.c
+++ C-Programming/Practise File Extra/Phitron_HackerRank_extra_practice/pattern.c
@@ -1,11 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int n; 
+int main()
+{
+    int n;
     scanf("%d", &n);
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=i; j++){
-            printf("%d",j);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    for (int i = n - 1; i >= 1; i--)
+    {
+        for (int s = 1; s <= n - i; s++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d", j);
         }
         printf("\n");
     }
