5c5b76aa72381e930e56d2bb7dfc95e676ac4554
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-11-02 11:16:36 +0600

    added

---
+++ C-Programming/Module_13/sorting.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        int j = i + 1;
+        if (arr[i] > arr[j])
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
