da27fded8672b5a8f2bda599fb0773e616d86eb7
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-11-02 07:33:20 +0600

    added

---
+++ C-Programming/Module_13/piramid.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int star = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for(int k=1; k<=n-i+1; k++){
+            printf("  ");
+        }
+        for (int j = 1; j <= star; j++)
+        {
+            printf("* ");
+        }
+        star += 2;
+        printf("\n");
+    }
+    return 0;
+}
