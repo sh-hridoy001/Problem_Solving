5b8581e0f98adb2eabdb51225a41e91d152eef4e
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-11-02 12:07:35 +0600

    added

---
+++ C-Programming/Practise File Extra/Pattern_printing/ulta_tri.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int j = 1; j <= n; j++)
+    {
+        for (int k = 1; k <= j; k++)
+        {
+            printf(" ");
+        }
+        for (int i = 1; i <= n - j + 1; i++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
