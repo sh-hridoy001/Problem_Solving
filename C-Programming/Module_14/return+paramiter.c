9dc9fa5af506c97e2e13c43e273ceea3d4c63bd0
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-11-03 11:01:43 +0600

    added

--- C-Programming/Module_14/return+paramiter.c
+++ C-Programming/Module_14/return+paramiter.c
@@ -4,10 +4,14 @@ int add(int a, int b)
 {
     return a + b;
 }
-
+int sub(int a, int b)
+{
+    return a - b;
+}
 int main()
 {
-    add(5, 6);
+    printf("%d\n", sub(5, 6));
+    printf("%d", add(5, 6));
 
     return 0;
 }
