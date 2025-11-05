2de583f5a5c6053e347350c3e6c5b6e5bf354252
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-11-05 06:54:30 +0600

    added

--- C-Programming/Module_15/ptr.c
+++ C-Programming/Module_15/ptr.c
@@ -8,6 +8,11 @@ int main()
 
     int *ptr = &x;
     printf("%p\n", ptr); // x variable er address -----------
-    printf("%p", &ptr); /// pointer variable er address-------
+    printf("%p\n", &ptr); /// pointer variable er address-------
+    printf("%d\n", *ptr); // pointer a j address ache oi address er value ------------
+
+    // x er value change korbo pointer diye-----
+    *ptr = 200;
+    printf("%d", x);
     return 0;
 }
