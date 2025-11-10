54fbbe9f20e0b2590917f384c8a6594fa52759a8
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-11-10 17:07:53 +0600

    added

---
+++ C-Programming/Module_17/print_arr.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void printArray(int arr[], int n, int i){
+    if(i==(n-1)){
+        return;
+    }
+    printf("%d ",arr[i]);
+    printArray(arr, n, i+1);
+}
+int main(){
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    
+    printArray(arr, n, 0);
+    return 0;
+}
