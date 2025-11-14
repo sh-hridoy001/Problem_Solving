16580e07781ad01a1cd6114bbc5426e824a0ad8d
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-11-14 22:44:55 +0600

    added

--- C-Programming/Module_18/secondary_diagonal_matrix.c
+++ C-Programming/Module_18/secondary_diagonal_matrix.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 int main()
 {
     int row, col;
     scanf("%d %d", &row, &col);
     int arr[row][col];
+
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < col; j++)
@@ -20,35 +20,28 @@ int main()
         {
             for (int j = 0; j < col; j++)
             {
-                if (i != j)
+                if (i + j != row - 1)
                 {
-                    if(arr[i][j] != 0){
-                        flag = 1 ;
+                    if (arr[i][j] != 0)
+                    {
+                        flag = 1;
                         break;
                     }
-                    
                 }
-                // if(i==j){
-
-                // }
-                // else if(arr[i][j] != 0)
-                // {
-                //     flag++;
-                // }
             }
         }
         if (flag == 0)
         {
-            printf("It's a diagonal Matrix");
+            printf("This is a secondary diagonal Matrix");
         }
         else
         {
-            printf("It's not a diagonal Matrix");
+            printf("This is not a secondary diagonal Matrix");
         }
     }
     else
     {
-        printf("It's not a diagonal Matrix");
+        printf("This is not a secondary diagonal Matrix");
     }
 
     return 0;
