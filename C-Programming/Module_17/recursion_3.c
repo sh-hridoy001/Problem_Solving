d16ce9a368d6120a362c0293b70fafc52cdee06e
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-11-14 11:15:15 +0600

    added

--- C-Programming/Module_17/recursion_3.c
+++ C-Programming/Module_17/recursion_3.c
@@ -2,18 +2,19 @@
 
 void printN(int n)
 {
-    if (n == 6)
+    if (n == 0)
     {
         return;
     }
-    printf("%d\n", n);
-    printN(n + 1);
+    printf("I love Recursion\n");
+    printN(n - 1);
 }
 
 int main()
 {
     // print 1 to N: --------
-    int i = 1;
-    printN(i);
+    int n;
+    scanf("%d", &n);
+    printN(n);
     return 0;
 }
