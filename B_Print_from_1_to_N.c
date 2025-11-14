ccb29622acb7b01ffbd13a3606522d9518b860b6
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-11-14 11:21:18 +0600

    added

---
+++ B_Print_from_1_to_N.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void printN(int i, int n)
+{
+    if (i == n+1)
+    {
+        return;
+    }
+    printf("%d\n", i);
+    printN(i+1, n);
+}
+int main()
+{
+    int i = 1, n;
+    scanf("%d", &n);
+    printN(i, n);
+    return 0;
+}
