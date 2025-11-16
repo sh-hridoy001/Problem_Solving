2ffc85f09366b671d92c94b2bef6f2f24e7f1851
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-11-16 10:06:23 +0600

    added

---
+++ C++/C++(With_Phitron)/Module_01/min_max_swap.cpp
@@ -0,0 +1,13 @@
+// author: Sazzad
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x = 10, y = 20;
+    cout << min(x, y) << endl;
+    cout << max(x, y) << endl;
+    swap(x, y);
+    cout << x << " " << y;
+    return 0;
+}
