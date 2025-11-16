ac4397954332fca7a273e6a770822fcaca199ff1
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-11-16 09:37:59 +0600

    added

---
+++ C++/C++(With_Phitron)/Module_01/setprecision.cpp
@@ -0,0 +1,14 @@
+// author: Sazzad
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin>> x;
+    float y ;
+    cin>> y ;
+    float xy = x*y ;
+    cout<<"Multiplication = "<<fixed <<setprecision(2) <<xy ; // number for printing after floating point ----
+    return 0;
+}
