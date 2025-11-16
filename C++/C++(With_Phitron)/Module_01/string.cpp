7f7056173b21131256a02c7b966b98f4ae522b6d
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-11-16 10:12:11 +0600

    added

---
+++ C++/C++(With_Phitron)/Module_01/string.cpp
@@ -0,0 +1,13 @@
+// author: Sazzad
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char str[100];
+    // cin >> str;    // string a space thakle eta kaj korbe na ,,,
+    cin.getline(str,100);
+    cout<< str ;
+
+    return 0;
+}
