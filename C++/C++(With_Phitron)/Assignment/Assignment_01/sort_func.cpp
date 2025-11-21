22803535d2d1e801bab911138804b9c369bdc937
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-11-21 09:47:37 +0600

    added

---
+++ C++/C++(With_Phitron)/Assignment/Assignment_01/sort_func.cpp
@@ -0,0 +1,27 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int *sort_it(int n)
+{
+    int* arr = new int[n];
+    for(int i=0; i<n; i++){
+        cin>> arr[i];
+    }
+    sort(arr, arr+n, greater<int>());
+    return arr;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int *x = sort_it(n);
+    for(int i=0; i<n; i++){
+        cout<<x[i] <<" ";
+    }
+
+    return 0;
+}
+
+//accepted*
