3638b0e957f48478148a7ccaade4559cca536c5f
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-11-21 22:58:11 +0600

    added

---
+++ C++/C++(With_Phitron)/Assignment/Assignment_01/Question_3_Who_is_it.cpp
@@ -0,0 +1,41 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    int id;
+    char name[100];
+    char section;
+    int total_mark;
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        Student x, y, z;
+        cin >> x.id >> x.name >> x.section >> x.total_mark;
+        cin >> y.id >> y.name >> y.section >> y.total_mark;
+        cin >> z.id >> z.name >> z.section >> z.total_mark;
+
+        Student best = x;
+        if (y.total_mark > best.total_mark ||
+            (y.total_mark == best.total_mark && y.id < best.id))
+        {
+            best = y;
+        }
+        if (z.total_mark > best.total_mark ||
+            (z.total_mark == best.total_mark && z.id < best.id))
+        {
+            best = z;
+        }
+        cout << best.id << " " << best.name << " " << best.section << " " << best.total_mark;
+        cout << endl;
+    }
+
+    return 0;
+}
