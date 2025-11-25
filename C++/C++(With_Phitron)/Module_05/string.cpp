5142465bdd1c550dfa80ea5bdd655180c5e80401
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-11-25 22:29:43 +0600

    added

---
+++ C++/C++(With_Phitron)/Module_05/string.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    // C-স্টাইল স্ট্রিং (C-Style String)
+
+    string s1 = "Hello"; //eta char array chilo --
+
+    cout << s1 << endl; // Output: Hello
+
+    // C++ স্ট্রিং (C++ String)
+
+    string s2 = "World";
+
+    cout << s2 << endl; // Output: World
+
+    // C++ স্ট্রিং অপারেশন (C++ String Operations)
+
+    string s3 = s1+" "+s2;
+
+    cout << s3 << endl; // Output: Hello World
+
+    return 0;
+}
---
+++ C++/Codeforces/Sheet#3(Array)/N_Check_Code.cpp
@@ -0,0 +1,37 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    string s;
+    cin >> s;
+    int len = s.length();
+
+    if (len != a + b + 1)
+    {
+        cout << "No" << endl;
+        return 0;
+    }
+    if (s[a] == '-')
+    {
+        for (int i = 0; i < len; i++)
+        {
+            if (i == a)
+            {
+                continue;
+            }
+            if (!isdigit(s[i]))
+            {
+                cout << "No\n";
+                return 0;
+            }
+        }
+        cout << "Yes";
+    }else{
+        cout<<"No";
+    }
+    return 0;
+}
