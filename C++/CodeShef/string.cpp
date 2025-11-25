a08e4e474c63ebb83852c2e25c8d90de1202c857
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-11-25 09:48:03 +0600

    added

---
+++ C++/CodeShef/string.cpp
@@ -0,0 +1,17 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string one = "Coding";
+    string two = "on";
+    string three = "CodeChef";
+    cout<<one<<" - "<<one.length()<<endl;
+    cout<<two<<" - "<<two.length()<<endl;
+    cout<<three<<" - "<<three.length()<<endl;
+    string four = one+" "+two+" "+three ;
+    cout<<four<<" - "<<four.length() ;
+
+    return 0;
+}
