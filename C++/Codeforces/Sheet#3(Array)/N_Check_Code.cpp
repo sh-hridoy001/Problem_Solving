97348865c69db14e4439ae4fa11a8094f1af7c02
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-11-25 22:31:47 +0600

    added

--- C++/Codeforces/Sheet#3(Array)/N_Check_Code.cpp
+++ C++/Codeforces/Sheet#3(Array)/N_Check_Code.cpp
@@ -30,8 +30,44 @@ int main()
             }
         }
         cout << "Yes";
-    }else{
-        cout<<"No";
+    }
+    else
+    {
+        cout << "No";
     }
     return 0;
 }
+//accepted * 
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+
+//     int a, b;
+//     cin >> a >> b;
+//     string s;
+//     cin >> s;
+//     int c = 0;
+//     int len = s.length();
+
+//     // checking - (highfen) ---
+//     if (s[a] != '-')
+//     {
+//         cout << "No" << endl;
+//         return 0;
+//     }
+
+//     for (int i = 0; i < len; i++)
+//     {
+//         if (i == a)
+//             continue;
+//         if (!isdigit(s[i]))
+//         {
+//             cout << "No" << endl;
+//             return 0;
+//         }
+//     }
+//     cout << "Yes" << endl;
+//     return 0;
+// }
