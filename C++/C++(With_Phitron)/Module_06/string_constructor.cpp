f13e57c03cdde24d66cb3327c9c3081a3d6e2e04
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-11-28 07:59:14 +0600

    added

---
+++ C++/C++(With_Phitron)/Module_06/string_constructor.cpp
@@ -0,0 +1,23 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{ // 1 number way ---
+    // string s = "Hello World";
+    // cout << s << endl;
+    // 2 number way ---
+
+    // string h("Hello Hridoy\n");
+    // cout << h;
+    // string i("Hello Hridoy\n", 4);
+    // cout << i;
+    // string x(h, 4);
+    // cout << x;
+    string h(5, 'Z');
+    cout<<h ;
+
+    // string s("Hello world", 4);
+    // cout<<s ;
+    return 0;
+}
