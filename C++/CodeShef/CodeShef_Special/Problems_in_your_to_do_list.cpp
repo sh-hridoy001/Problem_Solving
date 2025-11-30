471bc3c7bede6998c4f8957b04c853a0062c7dbe
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-11-30 12:13:21 +0600

    added

---
+++ C++/CodeShef/CodeShef_Special/Problems_in_your_to_do_list.cpp
@@ -0,0 +1,26 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+            if (arr[i] >= 1000)
+            {
+                count++;
+            }
+        }
+        cout << count << endl;
+    }
+    return 0;
+}
