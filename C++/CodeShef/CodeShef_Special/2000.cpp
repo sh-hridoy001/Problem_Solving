f21edc62e2697d437bf9a8fc48a50ad7fe69f060
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-12-07 10:46:46 +0600

    added

---
+++ C++/CodeShef/CodeShef_Special/2000.cpp
@@ -0,0 +1,12 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int amount_of_note = (n * 2000) / 500;
+    cout << amount_of_note;
+    return 0;
+}
