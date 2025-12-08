749679f46691c2922d4800c83cbdd80579f89ccd
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-12-08 10:35:09 +0600

    added

---
+++ Chef_and_Close_Friends.cpp
@@ -0,0 +1,17 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int x, y, z;
+        cin >> x >> y >> z;
+        int ans = 2 * min(y, z);
+        cout << ans << endl;
+    }
+    return 0;
+}
