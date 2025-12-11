99244f610707659c6cb6989f0eed91fcf4302f4a
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-12-11 18:44:13 +0600

    added

---
+++ Writing_Speed.cpp
@@ -0,0 +1,18 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n * 5 <= 60)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+    return 0;
+}
