23ac0cc42655ceee298538e6f978c89b21660f7e
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-12-14 19:29:35 +0600

    added

---
+++ DSA/Module_01/logarithmic_complexity.cpp
@@ -0,0 +1,16 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    // ekhane time complexity order of N ---0(N)--- 
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << endl;
+    }
+    return 0;
+}
