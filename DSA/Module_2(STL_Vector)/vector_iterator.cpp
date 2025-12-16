1315533ad2efa7e35b00a1b7751a66ea9a603dfb
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-12-16 20:17:21 +0600

    added

---
+++ DSA/Module_2(STL_Vector)/vector_iterator.cpp
@@ -0,0 +1,14 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v = {1,2,3,4,5};
+
+    // iterator diye print kortechi vector k -- -- 
+    for(auto it = v.begin(); it<v.end(); it++){
+        cout<<*it<<" ";
+    }
+    return 0;
+}
