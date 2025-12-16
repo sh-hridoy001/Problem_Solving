7cd0d354f89166451e3d1953585269aea33333ab
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-12-16 19:09:39 +0600

    added

--- DSA/Module_02/vectore_initialize.cpp
+++ DSA/Module_02/vectore_initialize.cpp
@@ -6,10 +6,13 @@ int main()
 {
     // vector<int> v;    //type - 1
     // vector<int> v(5);    // type - 2
-    vector<int> v(5, 1); // type - 3
-    int len = v.size();
+    // vector<int> v(5, 1); // type - 3
+    // int len = v.size();
+
+    int arr[5] = {1, 2, 2, 3, 4};
+    vector<int> v(arr, arr+5);
     // vector output : -
-    for (int i = 0; i < len; i++)
+    for (int i = 0; i < v.size(); i++)
     {
         cout << v[i] << " ";
     }
