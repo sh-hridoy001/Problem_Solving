e1b2c7233add7f3d2b983c0f71bf7fa56cd7fbfc
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-12-19 07:05:38 +0600

    added

---
+++ DSA/Module_3(Prefix_Sum&Binary_Search/practice-problem/Equilibrium-Index.cpp
@@ -0,0 +1,9 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
---
+++ DSA/Module_3(Prefix_Sum&Binary_Search/practice-problem/running_sum_of_an_array.cpp
@@ -0,0 +1,23 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n + 1);
+    vector<long long int> pre_v(n + 1);
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> v[i];
+        pre_v[i] = pre_v[i - 1] + v[i];
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        cout << pre_v[i] << " ";
+    }
+
+    return 0;
+}
