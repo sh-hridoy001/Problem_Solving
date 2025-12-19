a6bde5ff7d54a3d4dc254f22e62f178f04dad7cb
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-12-19 07:31:58 +0600

    added

---
+++ DSA/Module_3(Prefix_Sum&Binary_Search/practice-problem/G_Summation_from_1_to_N.cpp
@@ -0,0 +1,12 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n, sum;
+    cin >> n;
+    sum = n * (n + 1) / 2;
+    cout << sum;
+    return 0;
+}
