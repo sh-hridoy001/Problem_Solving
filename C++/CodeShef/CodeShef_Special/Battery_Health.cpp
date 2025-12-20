ef99f92168e50ad55f7c61860ae46b774aa6ec1a
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-12-20 11:24:10 +0600

    added

---
+++ C++/CodeShef/CodeShef_Special/Battery_Health.cpp
@@ -0,0 +1,23 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n;
+        cin >> n;
+        if (n >= 80)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}
