c15c10e12d16babd3c95a01f3597a71c301fc1a5
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-12-21 18:27:55 +0600

    added

---
+++ C++/CodeShef/CodeShef_Special/Tom_and_Jerry_Chase.cpp
@@ -0,0 +1,24 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int a, b;
+        cin >> a >> b;
+        if (a < b)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+
+    return 0;
+}
