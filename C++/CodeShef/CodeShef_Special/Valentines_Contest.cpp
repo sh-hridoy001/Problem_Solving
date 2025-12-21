d7126dd00c0751a6a48f0b1a72840d154060995c
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-12-21 18:36:07 +0600

    added

---
+++ C++/CodeShef/CodeShef_Special/Valentines_Contest.cpp
@@ -0,0 +1,11 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    n == 121 ? cout << "Likely" : cout << "Unlikely";
+    return 0;
+}
