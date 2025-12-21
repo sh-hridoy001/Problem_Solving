c02f145fa2d76cc4e8d062c6b32a2800c3cf1c6e
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-12-21 18:47:17 +0600

    added

---
+++ C++/CodeShef/CodeShef_Special/Waiting_Time.cpp
@@ -0,0 +1,17 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << (a * 7) - b << endl;
+    }
+
+    return 0;
+}
