c4ea9b772c62c3b1e87a1526732775f9e7ca3246
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-12-22 21:03:22 +0600

    added

---
+++ Mana_Points.cpp
@@ -0,0 +1,17 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << b / a << endl;
+    }
+
+    return 0;
+}
