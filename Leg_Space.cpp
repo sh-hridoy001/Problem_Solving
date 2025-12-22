d78c56e53eb24b17d37199b0df5db266fd7f1d7d
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-12-22 21:00:54 +0600

    added

---
+++ Leg_Space.cpp
@@ -0,0 +1,18 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if (a < b)
+    {
+        cout << "YES\n";
+    }
+    else
+    {
+        cout << "NO\n";
+    }
+    return 0;
+}
