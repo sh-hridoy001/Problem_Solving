da50806c92b5ae725f20dc38f683a06ed1ea02ce
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-12-24 11:58:56 +0600

    added

---
+++ DSA/Module_4(Singly_Linked_list)/dynamic_node.cpp
@@ -0,0 +1,29 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+
+    head->next = a;
+    a->next = b;
+    b->next = c;
+
+    cout << head->value << " " << head->next->value << " " << head->next->next->value << " " << head->next->next->next->value;
+    return 0;
+}
