67741df9bf3fee60e24f9efb2eb1260267a381cc
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-12-25 19:14:24 +0600

    added

---
+++ ICE_CREAM.cpp
@@ -0,0 +1,18 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if (a * 2 <= b)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+    return 0;
+}
