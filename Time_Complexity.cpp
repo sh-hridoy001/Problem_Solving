00ae79549adfe4c903cdad9f20de2e92e08ffafc
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-12-26 12:33:27 +0600

    added

---
+++ Time_Complexity.cpp
@@ -0,0 +1,23 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t, a, b;
+    cin >> t;
+    while (t--)
+    {
+        cin >> a >> b;
+        if (a > b)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+
+    return 0;
+}
