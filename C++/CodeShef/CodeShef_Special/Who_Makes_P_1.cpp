8f00bd73ee8760c90d783c9c534ab168ed7305c3
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-12-26 12:24:57 +0600

    added

---
+++ C++/CodeShef/CodeShef_Special/Who_Makes_P_1.cpp
@@ -0,0 +1,11 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    a > b ? cout << "Dom" : cout << "Tyro";
+    return 0;
+}
