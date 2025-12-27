986c389d62a31e5f6f2eee12a040897598505db8
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-12-27 19:48:48 +0600

    added

---
+++ DSA/Week_02/Module_06/delete_at_head.cpp
@@ -0,0 +1,51 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+void insert_at_tail(Node *&head, Node *&tail, int value)
+{
+    Node *newnode = new Node(value);
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+    }
+    tail->next = newnode;
+    tail = tail->next;
+}
+void delete_at_head(Node *&head)
+{
+    Node *delete_node = head;
+    head = head->next;
+    delete delete_node;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+    delete_at_head(head) ;
+
+    return 0;
+}
