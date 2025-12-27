cdb389271905b1812be7f5b258552d1449b71ee7
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-12-27 11:04:31 +0600

    added

---
+++ Donation_Rewards.cpp
@@ -0,0 +1,28 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        int a;
+        cin >> a;
+        if (a <= 3)
+        {
+            cout << "BRONZE\n";
+        }
+        else if (a >= 5 && a <= 6)
+        {
+            cout << "SILVER\n";
+        }
+        else
+        {
+            cout << "GOLD\n";
+        }
+    }
+
+    return 0;
+}
