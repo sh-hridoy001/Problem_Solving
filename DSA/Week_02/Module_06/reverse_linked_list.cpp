441879d240a0d9feca27845573fd3293f75a96a6
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-12-27 12:18:31 +0600

    added

---
+++ DSA/Week_02/Module_06/reverse_linked_list.cpp
@@ -0,0 +1,62 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int value)
+{
+    Node *newnode = new Node(value);
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+    }
+    tail->next = newnode;
+    tail = tail->next;
+}
+
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+}
+
+void reversed_print(Node*head){
+    
+}
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+    // insert_at_tail(head, tail,40);
+    // insert_at_tail(head, tail,50);
+    // insert_at_tail(head, tail,60);
+    
+    print_linked_list(head);
+    return 0;
+}
