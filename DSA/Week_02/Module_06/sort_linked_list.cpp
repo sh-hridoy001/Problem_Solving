bd250d2d8fb48cd6d97b3ab2d90ff92db13715f2
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-12-28 11:14:55 +0600

    added

---
+++ DSA/Week_02/Module_06/sort_linked_list.cpp
@@ -0,0 +1,72 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+// insert node --- -- --
+void insert_at_tail(Node *&head, Node *&tail, int value)
+{
+    Node *newnode = new Node(value);
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+    }
+    tail->next = newnode;
+    tail = tail->next;
+}
+
+void selection_sorting(Node *head)
+{
+    for (Node *i = head; i->next != NULL; i = i->next)
+    {
+        for (Node *j = i->next; j != NULL; j = j->next)
+        {
+            if (i->value > j->value)
+            {
+                swap(i->value, j->value);
+            }
+        }
+    }
+}
+
+// printing node ----
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+    selection_sorting(head);
+    print_linked_list(head);
+    return 0;
+}
