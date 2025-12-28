e9dfb46b570bd4b66e1380d2a0baacd77c52ce80
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-12-28 10:26:25 +0600

    added

---
+++ DSA/Week_02/Module_06/delete_at_any_pos.cpp
@@ -0,0 +1,65 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+void insert_at_tail(Node *&head, Node *&tail, int value)
+{
+    Node *newnode = new Node(value);
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+    }
+    tail->next = newnode;
+    tail = tail->next;
+}
+void delete_at_any_pos(Node *&head, int pos)
+{
+    Node* temp = head;
+    for(int i=1; i<pos; i++){
+        temp=temp->next;
+    }
+    Node* deletenode = temp->next;
+    temp->next = temp->next->next ;
+    delete deletenode ;
+}
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+}
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+    // int pos ;cin>> pos;
+    delete_at_any_pos(head, 1);
+
+    print_linked_list(head);
+    return 0;
+}
