e858f756dae3cf79cc82419c2eaeec4edad8e4fe
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-12-30 07:02:01 +0600

    added

---
+++ Volume_Control.cpp
@@ -0,0 +1,18 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc, a, b;
+    cin >> tc;
+    while (tc--)
+    {
+        cin >> a >> b;
+        cout << abs(a - b) << endl;
+    }
+
+    return 0;
+}
+
+//code explained -- -- --
