831a7cc793f2ededd1013ef6b972a2a419d4b563
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-01-01 11:50:40 +0600

    added

---
+++ Is_it_hot_or_cold.cpp
@@ -0,0 +1,24 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a;
+        cin >> a;
+        if (a > 20)
+        {
+            cout << "HOT\n";
+        }
+        else
+        {
+            cout << "COLD\n";
+        }
+    }
+
+    return 0;
+}
