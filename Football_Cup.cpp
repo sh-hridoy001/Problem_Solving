845c00c54c374f713ed11bd89ee3da1fff4cb740
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-01-02 11:33:31 +0600

    added

---
+++ Football_Cup.cpp
@@ -0,0 +1,24 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        if (a == b && a!=0)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+
+    return 0;
+}
