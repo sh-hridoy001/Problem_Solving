bc30a7a593ffc34500dc4cea4af009afbac637ea
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-01-05 20:16:12 +0600

    added

---
+++ Ezio_and_Guards.cpp
@@ -0,0 +1,24 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        if (a >= b)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+
+    return 0;
+}
