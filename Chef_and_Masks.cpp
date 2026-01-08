3b32123be369047ecf7b251bef2eb197349d6bd0
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-01-08 22:24:00 +0600

    added

---
+++ Chef_and_Masks.cpp
@@ -0,0 +1,24 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        if (a * 100 <= b * 10)
+        {
+            cout << "Cloth\n";
+        }
+        else
+        {
+            cout << "Disposable\n";
+        }
+    }
+
+    return 0;
+}
