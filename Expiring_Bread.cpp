0fc031210eca812661b8ca1f1e208ed7c1cc7dab
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-01-10 01:46:51 +0600

    added

---
+++ Expiring_Bread.cpp
@@ -0,0 +1,24 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m, k;
+        cin >> n >> m >> k;
+        if (n <= m * k)
+        {
+            cout << "Yes\n";
+        }
+        else
+        {
+            cout << "No\n";
+        }
+    }
+
+    return 0;
+}
