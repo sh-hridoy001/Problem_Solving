e4f78dc4ccbc4abf82e1a81cbabc66f12dd01283
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-01-14 20:43:29 +0600

    added

---
+++ Perfect_Trio.cpp
@@ -0,0 +1,24 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a + b == c || a + c == b || b + c == a)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+
+    return 0;
+}
