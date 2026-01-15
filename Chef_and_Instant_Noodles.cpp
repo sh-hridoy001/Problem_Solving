322b9f1a33d1c7b04ef4619dec5f910b53b3caa9
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-01-15 14:34:23 +0600

    added

---
+++ Chef_and_Instant_Noodles.cpp
@@ -0,0 +1,16 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t = 1;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << a * b;
+    }
+
+    return 0;
+}
