e77a131cda3403dca10b9edbcdb0b7d2e3d29930
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-01-15 20:50:03 +0600

    added

---
+++ DSA/Week_03/Module_8(Doubly_linked_List)/doubly_linked_list.cpp
@@ -0,0 +1,55 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node *prev;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void print_forward(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void print_backword(Node *tail)
+{
+    Node *temp = tail;
+    while (temp != NULL)
+    {
+        cout << temp->value << " ";
+        temp = temp->prev;
+    }
+}
+
+int main()
+{
+    Node *head = new Node(100);
+    Node *a = new Node(200);
+    Node *tail = new Node(300);
+
+    head->next = a;
+    a->prev = head;
+
+    a->next = tail;
+    tail->prev = a;
+
+    print_forward(head);
+    print_backword(tail);
+
+    return 0;
+}
