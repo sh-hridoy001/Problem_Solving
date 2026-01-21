28588134a97609f7f248673e5be9f3237f4bb781
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-01-21 21:47:16 +0600

    added

---
+++ DSA/Practice_Problem/Module_8_problem_sheet/sort_doubly.cpp
@@ -0,0 +1,49 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node *prev;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, int value, Node *&tail)
+{
+    Node *newnode = new Node(value);
+
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+    tail->next = newnode;
+    newnode->prev = tail;
+    tail = newnode;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, val, tail);
+    }
+    return 0;
+}
