fd530947ffa4dd2f109e4492801be6fc3b0d2952
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-01-21 12:04:27 +0600

    added

---
+++ DSA/Week_03/Module_8(Doubly_linked_List)/delete_at_head.cpp
@@ -0,0 +1,56 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node *prev;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void print_forward(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+void delete_at_head(Node *&head, Node *&tail)
+{
+    Node *deleteNode = head;
+    head = deleteNode->next;
+    delete deleteNode;
+    if (head == NULL)
+    {
+        tail = NULL;
+        return;
+    }
+    head->prev = NULL;
+}
+
+int main()
+{
+    Node *head = new Node(30);
+    Node *a = new Node(40);
+    Node *tail = new Node(20);
+
+    head->next = a;
+    a->prev = head;
+    a->next = tail;
+    tail->prev = a;
+
+    delete_at_head(head, tail);
+    delete_at_head(head, tail);
+    print_forward(head);
+    return 0;
+}
