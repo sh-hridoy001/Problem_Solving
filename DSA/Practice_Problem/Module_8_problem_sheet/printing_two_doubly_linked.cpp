339c28278b2291fa46146aba5c3de7a610a03338
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-01-21 17:24:43 +0600

    added

--- DSA/Practice_Problem/Module_8_problem_sheet/printing_two_doubly_linked.cpp
+++ DSA/Practice_Problem/Module_8_problem_sheet/printing_two_doubly_linked.cpp
@@ -40,29 +40,6 @@ void print_forward(Node *head)
     cout << endl;
 }
 
-bool sameOrNot(Node *head1, Node *head2)
-{
-    Node *temp1 = head1;
-    Node *temp2 = head2;
-    while (temp1 != NULL & temp2 != NULL)
-    {
-        if (temp1->value != temp2->value)
-        {
-            return false;
-        }
-        temp1 = temp1->next;
-        temp2 = temp2->next;
-    }
-    if (temp1 == NULL && temp2 == NULL)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
 int main()
 {
     Node *head1 = NULL;
@@ -89,14 +66,8 @@ int main()
         }
         insert_at_tail(head2, val, tail2);
     }
+    print_forward(head1);
+    print_forward(head2);
 
-    if (sameOrNot(head1, head2))
-    {
-        cout << "YES";
-    }
-    else
-    {
-        cout << "NO";
-    }
     return 0;
 }
