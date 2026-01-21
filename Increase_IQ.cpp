8f877a2b8faf9ffaa735c773d633fd193803be81
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-01-21 10:34:32 +0600

    added

---
+++ Increase_IQ.cpp
@@ -0,0 +1,16 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t = 1;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        (n + 7 > 170) ? cout << "Yes" : cout << "No";
+    }
+
+    return 0;
+}
