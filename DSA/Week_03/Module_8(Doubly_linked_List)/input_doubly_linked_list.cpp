bfec75d2c4a93c49c4dd101408bec2e33555e607
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-01-21 13:40:08 +0600

    added

---
+++ DSA/Week_03/Module_8(Doubly_linked_List)/input_doubly_linked_list.cpp
@@ -0,0 +1,60 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node *prev;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+int insert_at_tail(Node *&head, int value, Node *&tail)
+{
+    Node *newnode = new Node(value);
+    tail->next = newnode;
+    newnode->prev = tail;
+    tail = newnode;
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+    }
+}
+
+void print_forward(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    while (true)
+    {
+        int value;
+        cin >> value;
+        if (value == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, value, tail);
+    }
+    print_forward(head);
+    return 0;
+}
