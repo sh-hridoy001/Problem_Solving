764338c785a03eba34837c5a1badb67bff64cdc3
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-01-21 23:32:13 +0600

    added

---
+++ DSA/Week_03/Module_10_(STL_Cycle_detection)/list.cpp
@@ -0,0 +1,13 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    list<int> l(10, 3);
+    for (auto it = l.begin(); it != l.end(); it++)
+    {
+        cout << *it << endl;
+    }
+    return 0;
+}
