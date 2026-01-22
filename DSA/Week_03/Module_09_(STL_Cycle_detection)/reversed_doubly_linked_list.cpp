1ddfa06e223c4694f1969104547278e536d42d22
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-01-22 20:33:45 +0600

    added

---
+++ DSA/Week_03/Module_09_(STL_Cycle_detection)/reversed_doubly_linked_list.cpp
@@ -0,0 +1,68 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node *prev;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, int value, Node *&tail)
+{
+    Node *newnode = new Node(value);
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+    tail->next = newnode;
+    newnode->prev = tail;
+    tail = newnode;
+}
+
+void reversed(Node *head, Node *tail)
+{
+    for (Node *i = head, *j = tail; (i != j->next && i->prev != j); i = i->next, j = j->prev)
+    {
+        swap(i->value, j->value);
+    }
+}
+
+void print_forward(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, val, tail);
+    }
+    reversed(head, tail);
+    print_forward(head);
+    return 0;
+}
