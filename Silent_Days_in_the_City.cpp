8ecd852c693297ef735a5db3bde73692274abf0d
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-01-24 23:57:30 +0600

    added

--- Silent_Days_in_the_City.cpp
+++ Silent_Days_in_the_City.cpp
@@ -6,16 +6,26 @@ int main()
 {
     int n;
     cin >> n;
-    
+
     vector<int> v(n + 1);
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i < n; i++)
     {
         cin >> v[i];
     }
-
-    for (int i = 1; i <= n; i++)
+    int t;
+    cin >> t;
+    while (t--)
     {
-        cout << v[i] << " ";
+        int l, r;
+        int count = 0;
+        for (int i = l; i <= r; i++)
+        {
+            if (v[i] < v[i - 1] && v[i] < v[i + 1])
+            {
+                count++;
+            }
+        }
+        cout << count << endl;
     }
 
     return 0;
