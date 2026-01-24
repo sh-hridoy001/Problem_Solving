c2dfaaf608f8e880b728e491eb6431438594fead
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-01-24 23:52:32 +0600

    added

--- Silent_Days_in_the_City.cpp
+++ Silent_Days_in_the_City.cpp
@@ -5,7 +5,18 @@ using namespace std;
 int main()
 {
     int n;
-    cin>> n;
+    cin >> n;
     
+    vector<int> v(n + 1);
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> v[i];
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << v[i] << " ";
+    }
+
     return 0;
 }
