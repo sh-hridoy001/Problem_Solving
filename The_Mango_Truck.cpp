1db225334d6f55df23d1ba645308f7a87fd0568c
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-01-25 14:52:51 +0600

    added

--- The_Mango_Truck.cpp
+++ The_Mango_Truck.cpp
@@ -4,7 +4,15 @@ using namespace std;
 
 int main()
 {
-    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y, z;
+        cin >> x >> y >> z;
+        int total_mango = (z - y) / x;
+        cout << total_mango << endl;
+    }
+
     return 0;
 }
-// codeChef -- --
