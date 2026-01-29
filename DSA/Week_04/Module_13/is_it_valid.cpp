c6f98471b14c6ef880c1a95ec3d931c0326caf20
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-01-29 19:53:05 +0600

    added

---
+++ DSA/Week_04/Module_13/is_it_valid.cpp
@@ -0,0 +1,17 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    stack<string> st;
+    string s;
+    while (true)
+    {
+        cin >> s;
+        st.push(s);
+    }
+    
+
+    return 0;
+}
