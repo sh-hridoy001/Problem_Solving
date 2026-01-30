4cab5640e90329b9431dadc5f495eaa8422b55fe
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-01-31 00:35:38 +0600

    added

---
+++ DSA/Week_05/Module_15(Binary-Tree)/inOrder-tree.cpp
@@ -0,0 +1,45 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *left;
+    Node *right;
+    Node(int value)
+    {
+        this->value = value;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void inOrder(Node *root)
+{
+    if (root == NULL)
+        return;
+
+    inOrder(root->left);
+    cout << root->value << " ";
+    inOrder(root->right);
+}
+
+int main()
+{
+    Node *root = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+    Node *e = new Node(60);
+
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    b->left = d;
+    b->right = d;
+    inOrder(root);
+    return 0;
+}
