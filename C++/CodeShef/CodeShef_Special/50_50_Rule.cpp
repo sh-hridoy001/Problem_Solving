05f8ae87a89042b32767e1e4673cf389791a0b33
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-01-30 22:35:29 +0600

    added

--- C++/CodeShef/CodeShef_Special/50_50_Rule.cpp
+++ C++/CodeShef/CodeShef_Special/50_50_Rule.cpp
@@ -1,17 +1,21 @@
-//Author - sh_hridoy001 
+// Author - sh_hridoy001
 
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
 	int t;
-	cin>>t;
-	while(t--){
-	    int a,m;
-	    cin>>a>>m;
-	    if(a<50) cout<<"Z"<<endl;
-	    else if(m<50) cout<<"F"<<endl;
-	    else cout<<"A"<<endl;
+	cin >> t;
+	while (t--)
+	{
+		int a, m;
+		cin >> a >> m;
+		if (a < 50)
+			cout << "Z" << endl;
+		else if (m < 50)
+			cout << "F" << endl;
+		else
+			cout << "A" << endl;
 	}
-
 }
