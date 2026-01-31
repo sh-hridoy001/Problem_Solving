6677c40a4da5c3b31c273ff0daf9b3b447380142
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-01-31 20:07:28 +0600

    added

---
+++ Uncompleted_problem/E_Interval_Sweep.cpp
@@ -0,0 +1,17 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int l, r;
+    if (r - l + 1 >= 1)
+    {
+        cout << "YES\n";
+    }
+    else
+    {
+        cout << "NO\n";
+    }
+    return 0;
+}
