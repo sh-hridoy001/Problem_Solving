bf35e41fadda2a9bb18c46f6410eded6c3578a30
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-02-02 22:01:39 +0600

    added

---
+++ CRED_Coins.cpp
@@ -0,0 +1,18 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+        int c = x * y;
+        cout << c / 100 << endl;
+    }
+
+    return 0;
+}
