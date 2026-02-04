bf5469b5489a962969c109f9983596d42e7da2a4
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-02-04 12:35:05 +0600

    added

---
+++ DSA/Week_05/Module_17(Tree_Problem_Solving)/pair_array.cpp
@@ -0,0 +1,20 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<pair<int, int>> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i].first >> v[i].second;
+        swap(v[i].first , v[i].second);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i].first << " " << v[i].second << endl;
+    }
+    return 0;
+}
