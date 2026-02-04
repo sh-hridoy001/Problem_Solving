77aabe8cd6217c51e9742c1128344a0aba489901
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-02-04 12:31:22 +0600

    added

---
+++ DSA/Week_05/Module_17(Tree_Problem_Solving)/pair.cpp
@@ -0,0 +1,12 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    pair<int, int> p;
+    // p = make_pair(2, 3);
+    p = {2, 3}; // shortcut system to making pair -- --
+    cout << p.first << " " << p.second;
+    return 0;
+}
