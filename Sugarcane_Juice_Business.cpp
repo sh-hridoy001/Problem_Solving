7162935da89c1c666f053ff448a6fe4932fc44c3
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-02-10 21:46:25 +0600

    added

---
+++ Sugarcane_Juice_Business.cpp
@@ -0,0 +1,15 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t,n;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>n;
+	    cout<<(n*50*30)/100<<endl;
+	}
+    return 0;
+}
