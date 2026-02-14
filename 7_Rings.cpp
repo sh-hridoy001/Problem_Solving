802b8e55d109883ecde003893e42dd1cab29cc22
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-02-14 11:33:10 +0600

    added

--- 7_Rings.cpp
+++ 7_Rings.cpp
@@ -4,6 +4,22 @@ using namespace std;
 
 int main()
 {
-    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, result;
+        cin >> a >> b;
+        result = a * b;
+        if (result > 9999 && result < 99999)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+
     return 0;
 }
