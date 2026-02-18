41a73f3293ca7b701ee2408f1499bd9d465341bf
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-02-18 12:27:23 +0600

    added

---
+++ The_Three_Topics.cpp
@@ -0,0 +1,18 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (a == d || b == d || c == d)
+    {
+        cout << "Yes\n";
+    }
+    else
+    {
+        cout << "No\n";
+    }
+    return 0;
+}
