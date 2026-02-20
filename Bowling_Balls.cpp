24e1c6147693ad691e7996404f665817f4e766b8
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-02-20 23:08:49 +0600

    added

---
+++ Bowling_Balls.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int Tc;
+    cin >> Tc;
+
+    while (Tc--)
+    {
+        int N, X, Y;
+        cin >> N >> X >> Y;
+
+        vector<int> A(N);
+        for (int i = 0; i < N; ++i)
+        {
+            cin >> A[i];
+        }
+
+        int count = 0;
+        for (int i = 0; i < N; ++i)
+        {
+            if (A[i] >= X && A[i] <= Y)
+            {
+                count++;
+            }
+        }
+
+        cout << count << endl;
+    }
+
+    return 0;
+}
