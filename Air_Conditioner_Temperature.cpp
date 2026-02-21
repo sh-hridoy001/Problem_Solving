09f8c3db1db3aa6c8c94e926a14198b039d40e9d
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-02-21 22:34:22 +0600

    added

---
+++ Air_Conditioner_Temperature.cpp
@@ -0,0 +1,24 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (b >= a && b >= c)
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
+    
+    return 0;
+}
