0afa9cfedf2072fe642e5c0851ab5020f185925d
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-02-23 23:16:47 +0600

    added

---
+++ Nearest_Exit.cpp
@@ -0,0 +1,19 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int x;
+        cin >> x;
+        if (x <= 50)
+            cout << "LEFT" << endl;
+        else
+            cout << "RIGHT" << endl;
+    }
+    return 0;
+}
