aa56d28415feedbce5643f61115ce528c4d13cf0
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-03-02 22:29:08 +0600

    added

---
+++ Chocolate_Distribution.cpp
@@ -0,0 +1,16 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n;
+        cin >> n;
+        cout << (n + 1) / 2 << " " << n << endl;
+    }
+    return 0;
+}
