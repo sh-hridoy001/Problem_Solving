0e354764728d2de88cf8d1564efc55bbc98f17a2
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-03-07 15:15:36 +0600

    added

---
+++ DSA/Algorithm/Module_02/path_printing.cpp
@@ -0,0 +1,73 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> adj_list[1005];
+bool vis[1005];
+int level[1005];
+int parent[1005];
+
+void bfs(int src)
+{
+    queue<int> q;
+    q.push(src);
+    vis[src] = true;
+    level[src] = 0;
+
+    while (!q.empty())
+    {
+        // ber kore ana
+        int par = q.front();
+        q.pop();
+
+        // child niye kaj
+        for (int child : adj_list[par])
+        {
+            if (!vis[child])
+            {
+                q.push(child);
+                vis[child] = true;
+                level[child] = level[par] + 1;
+                parent[child] = par;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_list[b].push_back(a);
+        adj_list[a].push_back(b);
+    }
+    memset(vis, false, sizeof(vis));
+    memset(level, -1, sizeof(level));
+    memset(parent, -1, sizeof(parent));
+
+    int src, dst;
+    cin >> src >> dst;
+    bfs(src);
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << i << " parent-> " << parent[i] << endl;
+    // }
+
+    int node = dst;
+    vector<int> path;
+    while (node != -1)
+    {
+        path.push_back(node);
+        node = parent[node];
+    }
+    reverse(path.begin(), path.end());
+    for (int x : path)
+    {
+        cout << x << " ";
+    }
+    return 0;
+}
