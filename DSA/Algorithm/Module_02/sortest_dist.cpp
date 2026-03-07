1e0e5a203133de2e101d9a16807e2eef766cf992
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-03-07 14:50:12 +0600

    added

---
+++ DSA/Algorithm/Module_02/sortest_dist.cpp
@@ -0,0 +1,53 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> adj_list[1005];
+bool vis[1005];
+int level[1005];
+
+void bfs(int src)
+{
+    queue<int> q;
+    q.push(src);
+    vis[src] = true;
+    level[src] = 0;
+    while (!q.empty())
+    {
+        // ber kore ana
+        int par = q.front();
+        q.pop();
+
+        // child niye kaj
+        for (int child : adj_list[par])
+        {
+            if (!vis[child])
+            {
+                q.push(child);
+                vis[child] = true;
+                level[child] = level[par] + 1;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_list[b].push_back(a);
+        adj_list[a].push_back(b);
+    }
+    memset(vis, false, sizeof(vis));
+    memset(level, -1, sizeof(level));
+
+    int src, dst;
+    cin >> src >> dst;
+    bfs(src);
+    cout << level[dst];
+    return 0;
+}
