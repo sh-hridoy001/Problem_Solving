e5241958724a0b172bf48b3f379771c0daf36d27
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-03-08 21:28:43 +0600

    added

---
+++ DSA/Algorithm/Week_01/Module_03/dfs_on_2D_grid.cpp
@@ -0,0 +1,23 @@
+// Author:- SH_Hridoy
+#include <bits/stdc++.h>
+using namespace std;
+char grid[105][105];
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            cin >> grid[i][j];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << grid[i][j];
+        }
+        cout << endl;
+    }
+    return 0;
+}
