6d42a90a9756d8abcfac848d79f3feb18492ad12
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2026-03-08 22:14:26 +0600

    added

--- DSA/Algorithm/Week_01/Module_03/dfs_on_2D_grid.cpp
+++ DSA/Algorithm/Week_01/Module_03/dfs_on_2D_grid.cpp
@@ -1,23 +1,35 @@
 // Author:- SH_Hridoy
 #include <bits/stdc++.h>
 using namespace std;
+
 char grid[105][105];
+bool vis[105][105];
+vector<pair<int, int>> d = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+void dfs(int si, int sj)
+{
+    cout << si << " " << sj << endl;
+    vis[si][sj] = true;
+    for (int i = 0; i < 4; i++)
+    {
+        int ci, cj;
+        ci = si + d[i].first;
+        cj = sj - d[i].second;
+        cout << ci << " " << cj << endl;
+    }
+}
 
 int main()
 {
     int n, m;
     cin >> n >> m;
+
     for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++)
             cin >> grid[i][j];
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            cout << grid[i][j];
-        }
-        cout << endl;
-    }
+    int si, sj;
+    cin >> si >> sj;
+    dfs(si, sj);
     return 0;
 }
